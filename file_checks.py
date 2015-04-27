bfe547d981af405201a1e1d71dffa22552929310
Horea Christian <horea.christ@yandex.com>
2015-04-27 23:42:44 +0200

    removed old commented code, added value checking

--- file_checks.py
+++ file_checks.py
@@ -32,24 +32,21 @@ def read_header_dicom(filesdir, my_file=False):
 	meta = read_file(my_file)
 	print(meta)
 
-def check_dicom_header(filesdir, check_value):
+def check_dicom_header(filesdir, check_value=False, given_value=False, firstonly=False):
 	import nipype.interfaces.dcmstack as dcmstack
 	from dcmstack.extract import default_extractor
 	for dicom_file in listdir(filesdir):
 		meta = default_extractor(read_file(filesdir+dicom_file, stop_before_pixels=True, force=True))
-		meta = meta[check_value]	
-		if meta != 0.35:
-			print(dicom_file, "muie")
+		if check_value:
+			meta = meta[check_value]
+			if meta != given_value:
+				print(dicom_file, "not the given value", meta)
+		else:
+			print(meta)
+		if firstonly:
+			break
 
 if __name__ == "__main__":
-	# files_dir = "/home/chymera/data2/dc.rs/export_ME/nii/4459/1/EPI/"
-	# example_filelist = [files_dir+file_path for file_path in listdir(files_dir)]
-	# example_filelist_joanes=[
-	# 	"/home/chymera/src/RS/1eME.nii",
-	# 	"/home/chymera/src/RS/2eME.nii",
-	# 	"/home/chymera/src/RS/3eME.nii"
-	# ]
-	# read_header(example_filelist)
 	# read_header_dicom("/home/chymera/data2/dc.rs/export_ME/dicom/4459/1/EPI/")
-	check_dicom_header("/home/chymera/data2/dc.rs/export_ME/dicom/4459/1/EPI/", check_value="SliceThickness")
+	check_dicom_header("/home/chymera/data2/dc.rs/export_ME/dicom/4459/1/EPI/", firstonly=True)
 	# read_header_nii("/home/chymera/data2/dc.rs/export_ME/nii/4459/1/EPI/")

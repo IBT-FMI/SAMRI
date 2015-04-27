803d12487d3bfbe3326a524f9fc85d1edaf383a7
Horea Christian <horea.christ@yandex.com>
2015-04-27 14:09:20 +0200

    exporting to directory AND no longer explicitly creating directory

--- preprocessing_functions.py
+++ preprocessing_functions.py
@@ -1,17 +1,15 @@
-from nipype.interfaces.dcmstack import DcmStack
-stacker = DcmStack()
+import nipype.interfaces.dcmstack as dcmstack
 from dcmstack.extract import default_extractor
 from dicom import read_file
 from os import listdir, path, makedirs
-from shutil import move
 
-def convert_dcm_dir(dicom_dir, multi_epi=False):
-
+def convert_dcm_dir(dicom_dir, multi_epi_check=True):
 	nii_dir = dicom_dir.replace("dicom", "nii")
-	if not path.exists(nii_dir):
-		makedirs(nii_dir)
+	# if not path.exists(nii_dir):
+		# makedirs(nii_dir)
+	stacker = dcmstack.DcmStack()
 
-	if multi_epi:
+	if multi_epi_check:
 		dicom_files = listdir(dicom_dir)
 		echo_times=[]
 		for dicom_file in dicom_files:
@@ -21,21 +19,19 @@ def convert_dcm_dir(dicom_dir, multi_epi=False):
 		for echo_time in list(set(echo_times)):
 			echo_indices = [i for i, j in enumerate(echo_times) if j == echo_time]
 			stacker.inputs.embed_meta = True
-			file_name = "EPI"+str(echo_time)[:2]
-			destination_file_name = nii_dir+"/"
 			stacker.inputs.dicom_files = [dicom_dir+dicom_files[index] for index in echo_indices]
-			stacker.inputs.out_path = destination_file_name
-			result = stacker.run()
-			print(result.outputs.out_file)
+			stacker.inputs.out_path = nii_dir+"/"
 
 	else:
 		stacker.inputs.dicom_files = dicom_dir
 		file_name = "EPI"
 		stacker.inputs.out_format = file_name
-		result = stacker.run()
 		destination_file_name = nii_dir+"/"+file_name+".nii.gz"
 		move(result.outputs.out_file, destination_file_name)
-		print(destination_file_name)
+
+	result = stacker.run()
+	return nii_dir
 
 if __name__ == "__main__":
-	convert_dcm_dir("/home/chymera/data2/dc.rs/export_ME/dicom/4459/1/EPI/", multi_epi=True)
+	for nr in [4457, 4459, 4460]:
+		convert_dcm_dir("/home/chymera/data2/dc.rs/export_ME/dicom/"+str(nr)+"/1/EPI/")

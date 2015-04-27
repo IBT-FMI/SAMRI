bf7ed78a0d070a8d85cecf964094b167a1f09033
Horea Christian <h.chr@mail.ru>
2015-04-27 02:04:53 +0200

    updated header read function

--- nifti_read.py
+++ nifti_read.py
@@ -1,25 +1,24 @@
 from nibabel.nifti1 import Nifti1Header
 import nibabel as nib
+from os import listdir
 
-example_filename = "50001-T2S_EP_Feb2015_multi.nii.gz"
-example_filelist=[
-	"/home/chymera/src/RS/EPI23.nii.gz",
-	"/home/chymera/src/RS/EPI11.nii.gz",
-	"/home/chymera/src/RS/EPI17.nii.gz"
-]
-example_filelist_joanes=[
-	"/home/chymera/src/RS/1eME.nii",
-	"/home/chymera/src/RS/2eME.nii",
-	"/home/chymera/src/RS/3eME.nii"
-]
+def read_header(example_filelist, save_header=False):
+	for example_file in example_filelist:
+		img=nib.load(example_file)
+		print(example_file)
+		if save_header:
+			text_file = open("Output_joanes.txt", "w")
+			text_file.write(str(img.header))
+			text_file.close()
+		else:
+			print(img.header)
 
-# img = nib.load(example_filename)
-# print img.header
-
-for example_file in example_filelist_joanes:
-	img=nib.load(example_file)
-	text_file = open("Output_joanes.txt", "w")
-	text_file.write(str(img.header))
-	text_file.close()
-
-# hdr = Nifti1Header("50001-T2S_EP_Feb2015_multi.nii.gz")
+if __name__ == "__main__":
+	files_dir = "/home/chymera/data2/dc.rs/export_ME/nifti/4459/1/EPI/"
+	example_filelist = [files_dir+file_path for file_path in listdir(files_dir)]
+	example_filelist_joanes=[
+		"/home/chymera/src/RS/1eME.nii",
+		"/home/chymera/src/RS/2eME.nii",
+		"/home/chymera/src/RS/3eME.nii"
+	]
+	read_header(example_filelist)

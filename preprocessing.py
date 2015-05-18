2c1e6ebcd98462cf4ba1d24a69b98796ff225c29
Horea Christian <horea.christ@yandex.com>
2015-05-18 16:47:05 +0200

    using stacker from external functions file

--- preprocessing.py
+++ preprocessing.py
@@ -8,32 +8,23 @@ from dcmstack.extract import default_extractor
 from nipype.interfaces.nipy.preprocess import FmriRealign4d
 from os import listdir
 from dicom import read_file
+import numpy as np
+
+dcm_to_nii_imports = ["import nipype.interfaces.dcmstack as dcmstack",
+					"from dcmstack.extract import minimal_extractor",
+					"from dicom import read_file",
+					"from os import listdir, path, makedirs"]
 
 def preproc_workflow(data_dir, workflow_base=".", force_convert=False):
 
-	if "dicom" in data_dir:
-		stacker = pe.Node(interface=dcmstack.DcmStack(), name='stack')
-		dicom_files = listdir(data_dir)
-		echo_times=[]
-		for dicom_file in dicom_files:
-			meta = default_extractor(read_file(data_dir+dicom_file, stop_before_pixels=True, force=True))
-			echo_times += [meta["EchoTime"]]
+	dcm_to_nii_function = Function(input_names=["dicom_dir", "d5_key"],
+										output_names=['out_file'],
+										function=convert_dcm_to_nii,
+										imports=dcm_to_nii_imports)
 
-		for echo_time in list(set(echo_times)):
-			echo_indices = [i for i, j in enumerate(echo_times) if j == echo_time]
-			stacker.inputs.embed_meta = True
-			# destination_file_name = nii_dir+"/"
-			# stacker.inputs.out_path = destination_file_name
-			# result = stacker.run()
-			# print(result.outputs.out_file)
-			# stacker.inputs.dicom_files = [data_dir+dicom_files[index] for index in echo_indices]
-			stacker.inputs.dicom_files = [data_dir+dicom_files[index] for index in echo_indices]
-
-		# nii_dir = convert_dcm_dir(data_dir)
-	elif "nii" in data_dir:
-		nii_dir = data_dir
-	else:
-		raise RuntimeError("Format of files in path is ambiguous (we determine the format frm the path, and not the extension). Most likely your path contains both 'nii' and 'dicom'.")
+	stacker = pe.Node(name="dcm_to_nii", interface=dcm_to_nii_function)
+	stacker.inputs.dicom_dir = data_dir
+	stacker.inputs.d5_key = "EchoTime"
 
 	realigner = pe.Node(interface=FmriRealign4d(), name='realign')
 	realigner.inputs.tr = 1.5

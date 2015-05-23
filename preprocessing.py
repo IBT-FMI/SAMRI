a191fbfdb7f4d4e0e832d1d4ff98b630d88065c5
Horea Christian <horea.christ@yandex.com>
2015-05-23 07:01:51 +0200

    new struct realigner, changed naming of nodes

--- preprocessing.py
+++ preprocessing.py
@@ -1,24 +1,25 @@
 import nipype.interfaces.utility as util		# utility
 import nipype.pipeline.engine as pe				# pypeline engine
 from nipype.interfaces.nipy.preprocess import FmriRealign4d
+from nipype.interfaces.nipy import SpaceTimeRealigner
 from extra_interfaces import DcmToNii
 from nipype.interfaces.dcmstack import DcmStack
 import nipype.interfaces.io as nio
 from copy import deepcopy
 
-def preproc_workflow(data_dir, workflow_base=".", force_convert=False, source_pattern="", IDs=""):
+def preproc_workflow(workflow_base=".", force_convert=False, source_pattern="", IDs=""):
 	# make IDs strings
 	if "int" or "float" in str([type(ID) for ID in IDs]):
 		IDs = [str(ID) for ID in IDs]
 
 	#initiate the infosource node
-	infosource = pe.Node(interface=util.IdentityInterface(fields=['subject_id']), name="infosource")
+	infosource = pe.Node(interface=util.IdentityInterface(fields=['subject_id']), name="subject_info_source")
 	#define the list of subjects your pipeline should be executed on
 	infosource.iterables = ('subject_id', IDs)
 
 	#initiate the DataGrabber node with the infield: 'subject_id'
 	#and the outfield: 'func' and 'struct'
-	datasource = pe.Node(interface=nio.DataGrabber(infields=['subject_id'], outfields=['func', 'struct']), name = 'datasource')
+	datasource = pe.Node(interface=nio.DataGrabber(infields=['subject_id'], outfields=['func', 'struct']), name = 'data_source')
 	#to specify the location of the experiment folder
 	# datasource.inputs.base_directory = '~/experiment_folder'
 	#define the structure of the data folders and files.
@@ -29,19 +30,17 @@ def preproc_workflow(data_dir, workflow_base=".", force_convert=False, source_pa
 	datasource.inputs.template_args['struct'] = [['subject_id','anatomical']]
 	datasource.inputs.sort_filelist = True
 
-	struct_stacker = pe.Node(name="struct_stack", interface=DcmStack())
+	struct_stacker = pe.Node(name="stack_convert_structural", interface=DcmStack())
 
-	stacker = pe.Node(name="dcm_to_nii", interface=DcmToNii())
+	stacker = pe.Node(name="stack_convert_functional", interface=DcmToNii())
 	stacker.inputs.group_by = "EchoTime"
 
-	realigner = pe.Node(interface=FmriRealign4d(), name='realign')
+	realigner = pe.Node(interface=FmriRealign4d(), name='ralign_functional')
 	realigner.inputs.tr = 1.5
 	realigner.inputs.time_interp = True
 	realigner.inputs.slice_order = range(0,20)[::2]+range(0,20)[1::2]
-	struct_realigner = pe.Node(interface=FmriRealign4d(), name='struct_realign')
-	struct_realigner.inputs.tr = 1.5
-	struct_realigner.inputs.time_interp = True
-	struct_realigner.inputs.slice_order = range(0,20)[::2]+range(0,20)[1::2]
+
+	struct_realigner = pe.Node(interface=SpaceTimeRealigner(), name='realign_structural')
 
 	workflow = pe.Workflow(name='preproc')
 	workflow.base_dir = workflow_base
@@ -61,7 +60,6 @@ def preproc_workflow(data_dir, workflow_base=".", force_convert=False, source_pa
 
 if __name__ == "__main__":
 	IDs=[4457,4459]
-	data_dirs=["/home/chymera/data/dc.rs/export_ME/dicom/"+str(ID)+"/1/EPI/" for ID in IDs]
 	source_pattern="/home/chymera/data/dc.rs/export_ME/dicom/%s/1/%s/"
 
-	preproc_workflow(data_dirs, workflow_base="/home/chymera/data/dc.rs/export_ME/", source_pattern=source_pattern, IDs=IDs)
+	preproc_workflow(workflow_base="/home/chymera/data/dc.rs/export_ME/", source_pattern=source_pattern, IDs=IDs)

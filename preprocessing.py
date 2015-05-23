3fcfd1127435fe22243301e28ef0a0ec9c28c599
Horea Christian <h.chr@mail.ru>
2015-05-23 07:08:55 +0200

    reformatted naming, removed comments

--- preprocessing.py
+++ preprocessing.py
@@ -19,29 +19,23 @@ def preproc_workflow(workflow_base=".", force_convert=False, source_pattern="", 
 	#initiate the DataGrabber node with the infield: 'subject_id'
 	#and the outfield: 'func' and 'struct'
 	datasource = pe.Node(interface=nio.DataGrabber(infields=['subject_id'], outfields=['func', 'struct']), name = 'data_source')
-	#to specify the location of the experiment folder
-	# datasource.inputs.base_directory = '~/experiment_folder'
-	#define the structure of the data folders and files.
-	#Each '%s' will later be filled by a template argument.
 	datasource.inputs.template = source_pattern
 	#First way: define the arguments for the template '%s/%s.nii' for each field individual
 	datasource.inputs.template_args['func'] = [['subject_id', 'EPI']]
 	datasource.inputs.template_args['struct'] = [['subject_id','anatomical']]
 	datasource.inputs.sort_filelist = True
 
-	struct_stacker = pe.Node(name="stack_convert_structural", interface=DcmStack())
-
-	stacker = pe.Node(name="stack_convert_functional", interface=DcmToNii())
+	stacker = pe.Node(interface=DcmToNii(), name="stack_convert_functional")
 	stacker.inputs.group_by = "EchoTime"
+	struct_stacker = pe.Node(interface=DcmStack(), name="stack_convert_structural")
 
 	realigner = pe.Node(interface=FmriRealign4d(), name='ralign_functional')
 	realigner.inputs.tr = 1.5
 	realigner.inputs.time_interp = True
 	realigner.inputs.slice_order = range(0,20)[::2]+range(0,20)[1::2]
-
 	struct_realigner = pe.Node(interface=SpaceTimeRealigner(), name='realign_structural')
 
-	workflow = pe.Workflow(name='preproc')
+	workflow = pe.Workflow(name='Preprocessing')
 	workflow.base_dir = workflow_base
 
 	workflow.connect([

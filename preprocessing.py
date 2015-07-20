469e32fc3d8c13d030ff3ddf83e4362cc5e4cc25
Horea Christian <h.chr@mail.ru>
2015-07-20 02:28:56 +0200

    removed realign interface

--- preprocessing.py
+++ preprocessing.py
@@ -29,11 +29,6 @@ def preproc_workflow(workflow_base=".", force_convert=False, source_pattern="", 
 	stacker.inputs.group_by = "EchoTime"
 	struct_stacker = pe.Node(interface=DcmStack(), name="stack_convert_structural")
 
-	realigner = pe.Node(interface=FmriRealign4d(), name='realign_functional')
-	realigner.inputs.tr = 1.5
-	realigner.inputs.time_interp = True
-	realigner.inputs.slice_order = range(0,20)[::2]+range(0,20)[1::2]
-
 	workflow = pe.Workflow(name='Preprocessing')
 	workflow.base_dir = workflow_base
 
@@ -41,16 +36,11 @@ def preproc_workflow(workflow_base=".", force_convert=False, source_pattern="", 
 		(infosource, datasource, [('subject_id', 'subject_id')]),
 		(datasource, stacker, [('func', 'dcm_dir')]),
 		(datasource, struct_stacker, [('struct', 'dicom_files')]),
-		(stacker, realigner, [('nii_files', 'in_file')]),
 		])
-	print datasource.outputs
-	print datasource.outputs.func
-	print datasource.outputs.struct
 	workflow.write_graph()
 	workflow.run(plugin="MultiProc")
 
 if __name__ == "__main__":
 	IDs=[4457,4459]
-	source_pattern="/mnt/data7/NIdata/dc.rs/export_ME/dicom/%s/1/%s/"
-
-	preproc_workflow(workflow_base="/home/chymera/NIdata/dc.rs/export_ME/", source_pattern=source_pattern, IDs=IDs)
+	source_pattern="/mnt/data7/NIdata/export_ME/dicom/%s/1/%s/"
+	preproc_workflow(workflow_base="/home/chymera/NIdata/export_ME/", source_pattern=source_pattern, IDs=IDs)

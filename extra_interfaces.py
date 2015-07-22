6f7b284dd5e1ffb404fef41634d273d7274e6e80
Horea Christian <horea.christ@yandex.com>
2015-07-22 23:49:02 +0200

    parsing -e and -d in command line correctly

--- extra_interfaces.py
+++ extra_interfaces.py
@@ -1,4 +1,4 @@
-from nipype.interfaces.base import BaseInterface, BaseInterfaceInputSpec, traits, File, TraitedSpec, Directory, CommandLineInputSpec, CommandLine
+from nipype.interfaces.base import BaseInterface, BaseInterfaceInputSpec, traits, File, TraitedSpec, Directory, CommandLineInputSpec, CommandLine, InputMultiPath
 from nipype.utils.filemanip import split_filename
 
 import nibabel as nb
@@ -32,22 +32,22 @@ class DcmToNii(BaseInterface):
 
 
 class MEICAInputSpec(CommandLineInputSpec):
-	echo_files = traits.List(File(exists=True, mandatory=True))
-	group_by = traits.Str(desc='everything below this value will be set to zero', mandatory=False)
+	echo_files = traits.List(File(exists=True), argstr="-d %s", mandatory=True)
+	echo_times = traits.List(traits.Float(), desc='Echo times corresponding to the input files', mandatory=True, argstr="-e %s")
 
 class MEICAOutputSpec(TraitedSpec):
-	nii_files = traits.List(File(exists=True))
+	nii_files = File(exists=True)
 
 class MEICA(CommandLine):
-	input_spec = DcmToNiiInputSpec
-	output_spec = DcmToNiiOutputSpec
+	input_spec = MEICAInputSpec
+	output_spec = MEICAOutputSpec
+	_cmd = "meica.py"
 
-	def _run_interface(self, runtime):
-		from extra_functions import dcm_to_nii
-		dcm_dir = self.inputs.dcm_dir
-		group_by = self.inputs.group_by
-		self.result = dcm_to_nii(dcm_dir, group_by, node=True)
-		return runtime
+	def _format_arg(self, name, spec, value):
+
+		if name in ["echo_files", "echo_times"]:
+			return spec.argstr % ",".join(map(str, value))
+		return super(MEICA, self)._format_arg(name, spec, value)
 
 	def _list_outputs(self):
 		outputs = self._outputs().get()

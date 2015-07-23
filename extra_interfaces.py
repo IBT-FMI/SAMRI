4dd9d83cc309043221c8140131fd7ac91b2c4673
Horea Christian <h.chr@mail.ru>
2015-07-23 03:21:57 +0200

    added support for more arguments

--- extra_interfaces.py
+++ extra_interfaces.py
@@ -32,8 +32,16 @@ class DcmToNii(BaseInterface):
 
 
 class MEICAInputSpec(CommandLineInputSpec):
-	echo_files = traits.List(File(exists=True), argstr="-d %s", mandatory=True)
-	echo_times = traits.List(traits.Float(), desc='Echo times corresponding to the input files', mandatory=True, argstr="-e %s")
+	echo_files = traits.List(File(exists=True), mandatory=True, position=0, argstr="-d %s", desc="4D files, for each echo time (called DSINPUTS by meica.py)")
+	echo_times = traits.List(traits.Float(), mandatory=True, position=1, argstr="-e %s", desc='Echo times (in ms) corresponding to the input files (called TES by meica.py)')
+	anatomical_dataset = File(exists=True, argstr="-a%s", desc='ex: -a mprage.nii.gz  Anatomical dataset (optional)')
+	basetime = traits.Str(argstr="-b %s", desc="ex: -b 10s OR -b 10v  Time to steady-state equilibration in seconds(s) or volumes(v). Default 0.")
+	wrap_to_mni = traits.Bool(False, usedefault=True, argstr='--MNI', desc="Warp to MNI space using high-resolution template")
+	TR = traits.Float(argstr="--TR=%s", desc='The TR. Default read from input dataset header')
+	tpattern = traits.Str(argstr="--tpattern=%s", desc='Slice timing (i.e. alt+z, see 3dTshift -help). Default from header. (N.B. This is important!)')
+	cpus = traits.Int(argstr="--cpus=%d", desc=' Maximum number of CPUs (OpenMP threads) to use. Default 2.')
+	no_despike = traits.Bool(False, usedefault=True, argstr='--no_despike', desc="Do not de-spike functional data. Default is to despike, recommended.")
+	qwarp = traits.Bool(False, usedefault=True, argstr='--no_despike', desc=" Nonlinear anatomical normalization to MNI (or --space template) using 3dQWarp, after affine")
 
 class MEICAOutputSpec(TraitedSpec):
 	nii_files = File(exists=True)

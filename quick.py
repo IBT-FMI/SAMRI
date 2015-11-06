34241d009aea66ff599e2b2b5fc4dad2747cf405
Horea Christian <h.chr@mail.ru>
2015-11-06 03:18:54 +0100

    first working version

--- quick.py
+++ quick.py
@@ -1,15 +1,24 @@
-import nipype.pipeline.engine as pe				# pypeline engine
+import nipype.pipeline.engine as pe
 from extra_interfaces import Bru2
+from nipype.interfaces.fsl import MELODIC
+from os import path
 
-def quick_melodic(workflow_base=".", series_dir):
+def quick_melodic(series_dir, workflow_base="."):
+	workflow_base = path.expanduser(workflow_base)
+	series_dir = path.expanduser(series_dir)
+
 	Bru2_converter = pe.Node(interface=Bru2(), name="convert_resize")
 	Bru2_converter.inputs.input_dir = series_dir
 
 	melodic = pe.Node(interface=MELODIC(), name="MELODIC")
-	melodic.report = True
+	melodic.inputs.report = True
+	melodic.inputs.dim = 10
+
+	workflow = pe.Workflow(name='QuickMELODIC')
+	workflow.base_dir = workflow_base
 
 	workflow.connect([
-		(Bru2_converter, melodic, [('nii_files', 'in_files')])
+		(Bru2_converter, melodic, [('nii_file', 'in_files')])
 		])
 
 	workflow.write_graph(graph2use="orig")
@@ -17,4 +26,4 @@ def quick_melodic(workflow_base=".", series_dir):
 
 if __name__ == "__main__":
 	series_dir="~/NIdata/ofM.dr/20151027_141609_4011_ofM_1_1/16"
-	preproc_workflow(workflow_base="~/NIdata/ofM.dr/", series_dir=series_dir)
+	quick_melodic(workflow_base="~/NIdata/ofM.dr/", series_dir=series_dir)

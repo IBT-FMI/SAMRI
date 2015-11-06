450d949bbc081732e87ca9ad80090c22ffa529b8
Horea Christian <horea.christ@yandex.com>
2015-11-06 05:07:11 +0100

    streamlined CBV scan selection

--- quick.py
+++ quick.py
@@ -1,29 +1,53 @@
 import nipype.pipeline.engine as pe
-from extra_interfaces import Bru2
+import nipype.interfaces.utility as util		# utility
+from extra_interfaces import Bru2, FindScan
 from nipype.interfaces.fsl import MELODIC
-from os import path
+from os import path, listdir
+import nipype.interfaces.io as nio
 
-def quick_melodic(series_dir, workflow_base="."):
+def quick_melodic(workflow_base="."):
 	workflow_base = path.expanduser(workflow_base)
-	series_dir = path.expanduser(series_dir)
+	IDs=[]
+	for sub_dir in listdir(workflow_base):
+		if sub_dir[:3] == "201":
+			IDs.append(sub_dir)
 
-	Bru2_converter = pe.Node(interface=Bru2(), name="convert_resize")
-	Bru2_converter.inputs.input_dir = series_dir
+	infosource = pe.Node(interface=util.IdentityInterface(fields=['measurement_id']), name="measurement_info_source")
+	#define the list of subjects your pipeline should be executed on
+	infosource.iterables = ('measurement_id', IDs)
+
+	datasource1 = pe.Node(interface=nio.DataGrabber(infields=['measurement_id'], outfields=['measurement_path']), name='data_source1')
+	datasource1.inputs.template = workflow_base+"/%s"
+	datasource1.inputs.template_args['measurement_id'] = [['measurement_id']]
+	datasource1.inputs.sort_filelist = True
+
+	find_scan = pe.Node(interface=FindScan(), name="find_scan")
+	find_scan.inputs.query = "7_EPI_CBV"
+	find_scan.inputs.query_file = "acqp"
+
+	Bru2_converter = pe.MapNode(interface=Bru2(), name="convert_resize", iterfield=['input_dir'])
 
 	melodic = pe.Node(interface=MELODIC(), name="MELODIC")
 	melodic.inputs.report = True
-	melodic.inputs.dim = 10
+	melodic.inputs.dim = 8
 
+	datasink = pe.Node(nio.DataSink(), name='sinker')
+	datasink.inputs.base_directory = workflow_base+"/"+"OUTPUTS"
+
+	#SET UP WORKFLOW:
 	workflow = pe.Workflow(name='QuickMELODIC')
 	workflow.base_dir = workflow_base
 
 	workflow.connect([
-		(Bru2_converter, melodic, [('nii_file', 'in_files')])
+		(infosource, datasource1, [('measurement_id', 'measurement_id')]),
+		(datasource1, find_scan, [('measurement_path', 'scans_directory')]),
+		(find_scan, Bru2_converter, [('positive_scans', 'input_dir')]),
+		(Bru2_converter, melodic, [('nii_file', 'in_files')]),
+		(melodic, datasink, [('report_dir', 'reports')])
 		])
 
 	workflow.write_graph(graph2use="orig")
 	workflow.run(plugin="MultiProc")
 
 if __name__ == "__main__":
-	series_dir="~/NIdata/ofM.dr/20151027_141609_4011_ofM_1_1/16"
-	quick_melodic(workflow_base="~/NIdata/ofM.dr/", series_dir=series_dir)
+	quick_melodic(workflow_base="~/NIdata/ofM.dr/")

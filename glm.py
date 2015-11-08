85f81eeeddee68e9c9023b0c37df400611e9672a
Horea Christian <h.chr@mail.ru>
2015-11-08 03:35:58 +0100

    renamed anatomical to structural

--- glm.py
+++ glm.py
@@ -2,9 +2,9 @@ import nipype.pipeline.engine as pe
 import nipype.interfaces.utility as util
 from nipype.interfaces.fsl import GLM
 
-def fsl_glm(workflow_base, functional_scan_type, anatomical_scan_type, workflow_denominator="FSL_GLM", omit_ID=[]):
+def fsl_glm(workflow_base, functional_scan_type, structural_scan_type, workflow_denominator="FSL_GLM", omit_ID=[]):
 	workflow_base = path.expanduser(workflow_base)
-	bg_preproc_workflow = bg_preproc(workflow_base, functional_scan_type, anatomical_scan_type=anatomical_scan_type, omit_ID=omit_ID)
+	bg_preproc_workflow = bg_preproc(workflow_base, functional_scan_type, structural_scan_type=structural_scan_type, omit_ID=omit_ID)
 
 	melodic = pe.Node(interface=MELODIC(), name="MELODIC")
 	melodic.inputs.report = True
@@ -31,4 +31,4 @@ def fsl_glm(workflow_base, functional_scan_type, anatomical_scan_type, workflow_
 	pipeline.run(plugin="MultiProc")
 
 if __name__ == "__main__":
-	quick_melodic(workflow_base="~/NIdata/ofM.dr/", functional_scan_type="7_EPI_CBV", anatomical_scan_type="4_T2_TurboRARE", omit_ID=["measurement_id_20151026_135856_4006_1_1"])
+	quick_melodic(workflow_base="~/NIdata/ofM.dr/", functional_scan_type="7_EPI_CBV", structural_scan_type="4_T2_TurboRARE", omit_ID=["measurement_id_20151026_135856_4006_1_1"])

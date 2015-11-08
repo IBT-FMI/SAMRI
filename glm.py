03bab065ee3285fa4a85234350855680cd922d75
Horea Christian <horea.christ@yandex.com>
2015-11-08 03:32:55 +0100

    dummy analysis (copied from quick.py)

--- glm.py
+++ glm.py
@@ -2,9 +2,33 @@ import nipype.pipeline.engine as pe
 import nipype.interfaces.utility as util
 from nipype.interfaces.fsl import GLM
 
-def fsl_glm(workflow_base, functional_scan_type, workflow_denominator="FSL_GLM", omit_ID=[]):
+def fsl_glm(workflow_base, functional_scan_type, anatomical_scan_type, workflow_denominator="FSL_GLM", omit_ID=[]):
 	workflow_base = path.expanduser(workflow_base)
-	bg_preproc_workflow = bg_preproc(workflow_base, functional_scan_type="", omit_ID=omit_ID)
+	bg_preproc_workflow = bg_preproc(workflow_base, functional_scan_type, anatomical_scan_type=anatomical_scan_type, omit_ID=omit_ID)
+
+	melodic = pe.Node(interface=MELODIC(), name="MELODIC")
+	melodic.inputs.report = True
+	melodic.inputs.dim = 8
+
+	datasink = pe.Node(nio.DataSink(), name='sinker')
+	datasink.inputs.base_directory = workflow_base+"/"+workflow_denominator+"_results"
+
+	#SET UP ANALYSIS WORKFLOW:
+	analysis_workflow = pe.Workflow(name="ICA")
+
+	analysis_workflow.connect([
+		(melodic, datasink, [('report_dir', 'MELODIC_reports')])
+		])
+
+	#SET UP COMBINED WORKFLOW:
+	pipeline = pe.Workflow(name=workflow_denominator+"_work")
+	pipeline.base_dir = workflow_base
+
+	pipeline.connect([(bg_preproc_workflow, analysis_workflow, [('bru2_convert.nii_file','MELODIC.in_files')])
+		])
+
+	pipeline.write_graph(graph2use="orig")
+	pipeline.run(plugin="MultiProc")
 
 if __name__ == "__main__":
-	quick_melodic(workflow_base="~/NIdata/ofM.dr/", functional_scan_type="7_EPI_CBV", omit_ID=["measurement_id_20151026_135856_4006_1_1"])
+	quick_melodic(workflow_base="~/NIdata/ofM.dr/", functional_scan_type="7_EPI_CBV", anatomical_scan_type="4_T2_TurboRARE", omit_ID=["measurement_id_20151026_135856_4006_1_1"])

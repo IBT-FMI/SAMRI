d8772869ab56fcd04d654ccb4f617b8d17ec887e
Horea Christian <horea.christ@yandex.com>
2016-02-16 23:07:49 +0100

    implemented single-measurement-compatible pipeline

--- quick.py
+++ quick.py
@@ -7,9 +7,26 @@ from preprocessing import bru_preproc_lite
 import nipype.interfaces.io as nio
 import shutil
 
-def quick_melodic(workflow_base, functional_scan_type, tr=1, conditions=[], workflow_denominator="QuickMELODIC", subjects_include=[], subjects_exclude=[], measurements_exclude=[]):
-	workflow_base = path.expanduser(workflow_base)
-	bru_preproc_workflow = bru_preproc_lite(workflow_base, functional_scan_type, tr=tr, conditions=conditions, subjects_include=subjects_include, subjects_exclude=subjects_exclude, measurements_exclude=measurements_exclude)
+# set of files by which to identify a  Bruker measurement directory
+bruker_files = {"AdjStatePerStudy", "ResultState", "subject"}
+
+def quick_melodic(measurements_base, functional_scan_type, workflow_base=False, tr=1, conditions=[], workflow_denominator="QuickMELODIC", include_subjects=[], exclude_subjects=[], exclude_measurements=[], include_measurements=[], debug_mode=False):
+
+	#make measurements_base absolute (this has to be here to allow the check below)
+	measurements_base = path.expanduser(measurements_base)
+
+	#check if a bruker measurement directory was specified as the measurements_base. If so, set the root directories one level up and add the basename to include_measurements
+	if bruker_files.issubset(listdir(measurements_base)):
+		include_measurements = [path.basename(measurements_base)]
+		measurements_base += "/.."
+
+	#make workflow_base absolute (this has to be here to catch the measurements_base change that might have occured earlier)
+	if workflow_base:
+		workflow_base = path.expanduser(workflow_base)
+	else:
+		workflow_base = measurements_base
+
+	bru_preproc_workflow = bru_preproc_lite(measurements_base, functional_scan_type, tr=tr, conditions=conditions, include_subjects=include_subjects, exclude_subjects=exclude_subjects, exclude_measurements=exclude_measurements, include_measurements=include_measurements)
 
 	melodic = pe.Node(interface=MELODIC(), name="melodic")
 	melodic.inputs.tr_sec = tr
@@ -42,4 +59,4 @@ def quick_melodic(workflow_base, functional_scan_type, tr=1, conditions=[], work
 		shutil.rmtree(workflow_base+"/"+workflow_denominator+"_work")
 
 if __name__ == "__main__":
-	quick_melodic(workflow_base="~/NIdata/ofM.dr/", functional_scan_type="7_EPI_CBV", conditions=["ofM"], subjects_include=[], subjects_exclude=[], measurements_exclude=["20151026_135856_4006_1_1", "20151027_121613_4013_1_1"])
+	quick_melodic("~/NIdata/ofM.dr/20151027_141609_4011_1_1", "7_EPI_CBV", conditions=[], include_subjects=[], exclude_subjects=[], exclude_measurements=["20151026_135856_4006_1_1", "20151027_121613_4013_1_1"], debug_mode=True)

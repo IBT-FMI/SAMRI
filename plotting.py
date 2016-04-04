9b739164a4e74510ee0f06695ebc3d342b03261c
Horea Christian <h.chr@mail.ru>
2016-04-04 18:07:00 +0200

    deleted cruft

--- plotting.py
+++ plotting.py
@@ -5,6 +5,9 @@ from nilearn import image, plotting
 def plot_nii(file_path, slices):
 	plotting.plot_anat(file_path, cut_coords=slices, display_mode="y", annotate=False, draw_cross=False)
 
+def plot_timecourse(file_path, roi_path=None, roi_number=None):
+	
+
 if __name__ == '__main__':
 	plot_nii("/home/chymera/FSL_GLM_work/GLM/_measurement_id_20151103_213035_4001_1_1/structural_cutoff/6_restore_maths.nii.gz", (-50,20))
 	plotting.show()

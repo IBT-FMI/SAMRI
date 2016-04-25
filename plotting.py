28e5c0bc2a5d3e38f8cab609c26d380bf7275c4b
Horea Christian <horea.christ@yandex.com>
2016-04-25 06:36:58 +0200

    testing design matirx

--- plotting.py
+++ plotting.py
@@ -1,13 +1,25 @@
 import os
 import nibabel
 from nilearn import image, plotting
+import pandas as pd
+
+import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.style.use('ggplot')
 
 def plot_nii(file_path, slices):
 	plotting.plot_anat(file_path, cut_coords=slices, display_mode="y", annotate=False, draw_cross=False)
 
-def plot_timecourse(file_path, roi_path=None, roi_number=None):
-	
+# def plot_timecourse(file_path, roi_path=None, roi_number=None):
+
+def plot_fsl_design(file_path):
+	df = pd.read_csv(file_path, skiprows=5, sep="\t", header=None, names=[1,2,3,4,5,6], index_col=False)
+	print(df)
+	df.plot()
+
+
 
 if __name__ == '__main__':
-	plot_nii("/home/chymera/FSL_GLM_work/GLM/_measurement_id_20151103_213035_4001_1_1/structural_cutoff/6_restore_maths.nii.gz", (-50,20))
-	plotting.show()
+	# plot_nii("/home/chymera/FSL_GLM_work/GLM/_measurement_id_20151103_213035_4001_1_1/structural_cutoff/6_restore_maths.nii.gz", (-50,20))
+	plot_fsl_design("/home/chymera/NIdata/ofM.dr/level1/first_level/_condition_ofM_subject_4001/modelgen/run0.mat")
+	plt.show()

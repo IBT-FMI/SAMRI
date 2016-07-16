ca291a9eb1616ca75d47d25afe02cac190af6f1f
Horea Christian <h.chr@mail.ru>
2016-07-17 00:09:41 +0200

    removed resolved test file

--- 3dblur_test.py
+++
@@ -1,7 +0,0 @@
-from  nipype.interfaces.afni.preprocess import BlurToFWHM
-blur = BlurToFWHM()
-blur.inputs.in_file = '/home/chymera/utils/3dblur/trans_10.nii.gz'
-blur.inputs.out_file = '/home/chymera/utils/3dblur/b_10.nii.gz'
-blur.inputs.fwhm = 5.6
-print blur.cmdline
-blur.run()

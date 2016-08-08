7a8f915a042001cac8821b98cb26a631e24bb53a
Horea Christian <horea.christ@yandex.com>
2016-08-08 03:57:18 +0200

    trying to create install script

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+	name="chyMRI",
+	version="",
+	description = "Animal fMRI pipelines",
+	author = "Horea Christian",
+	author_email = "h.chr@mail.ru",
+	url = "https://github.com/TheChymera/chyMRI",
+	keywords = ["fMRI", "pipelines", "data analysis"],
+	classifiers = [],
+	install_requires = [],
+	provides = ["chyMRI"],
+	entry_points = {'console_scripts' : \
+			['chyMRI = chyMRI.cli:main']
+		}
+	)

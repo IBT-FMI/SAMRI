c026d3b4c2afa670ab519c4f8add1077db74e949
Horea Christian <horea.christ@yandex.com>
2016-09-06 03:43:29 +0200

    added file to store analysis launch scripts

---
+++ SAMRI/plotting/run.py
@@ -0,0 +1,10 @@
+import maps, timeseries
+
+def blur_kernel_compare(conditions=["ofM","ofM_aF","ofM_cF1","ofM_cF2","ofM_pF"], parameters=["level2_dgamma","level2_dgamma_blurxy4","level2_dgamma_blurxy5", "level2_dgamma_blurxy6", "level2_dgamma_blurxy7"]):
+	for condition in conditions:
+		stat_maps = ["~/NIdata/ofM.dr/GLM/"+parameter+"/_category_multi_"+condition+"/flameo/mapflow/_flameo0/stats/tstat1.nii.gz" for parameter in parameters]
+		titles = [stat_map[32:-43] for stat_map in stat_maps]
+		maps.stat(stat_maps, cbv=True, cut_coords=(-49,8,43), threshold=3, interpolation="gaussian", save_as="~/"+condition+".pdf", figure_title=condition, subplot_titles=parameters)
+
+if __name__ == '__main__':
+	blur_kernel_compare()

8af739cc0e2f64957390710e2c68fe537ec88d82
Horea Christian <horea.christ@yandex.com>
2016-10-24 23:19:11 +0200

    moved example code to appropriate location

--- samri/plotting/examples.py
+++ samri/plotting/examples.py
@@ -1,5 +1,15 @@
 import maps, timeseries, dcm
 
+def responder_overview():
+	stat_maps = [
+		"/home/chymera/NIdata/ofM.dr/l2/subjectwise/4001/tstat1.nii.gz",
+		"/home/chymera/NIdata/ofM.dr/l2/subjectwise/4007/tstat1.nii.gz",
+		"/home/chymera/NIdata/ofM.dr/l2/subjectwise/4008/tstat1.nii.gz",
+		"/home/chymera/NIdata/ofM.dr/l2/subjectwise/4009/tstat1.nii.gz",
+		"/home/chymera/NIdata/ofM.dr/l2/subjectwise/4012/tstat1.nii.gz",
+		]
+	maps.stat(stat_maps, cbv=True, template="~/NIdata/templates/ds_QBI_chr.nii.gz", threshold=2.5, interpolation="gaussian", figure_title="Non/Responders",subplot_titles=["4001","4007","4008","4009","4012"])
+
 def blur_kernel_compare_dr(conditions=["ofM","ofM_aF","ofM_cF1","ofM_cF2","ofM_pF"], parameters=["level2_dgamma","level2_dgamma_blurxy4","level2_dgamma_blurxy5", "level2_dgamma_blurxy6", "level2_dgamma_blurxy7"], threshold=3):
 	from matplotlib.backends.backend_pdf import PdfPages
 	pp = PdfPages('/home/chymera/DR.pdf')
@@ -16,4 +26,5 @@ def blur_kernel_compare_erc(scan_types=["jin6","jin10","jin20","jin40","jin60","
 		maps.stat(stat_maps, cbv=True, threshold=threshold, interpolation="none", template="~/NIdata/templates/hires_QBI_chr.nii.gz", save_as="~/"+scan_type+".pdf", figure_title=scan_type, subplot_titles=parameters)
 
 if __name__ == '__main__':
-	blur_kernel_compare_dr()
+	# blur_kernel_compare_dr()
+	responder_overview()

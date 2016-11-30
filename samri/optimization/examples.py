a35c480cfc86a250af0b560bf9d07a25c68752b4
Horea Christian <horea.christ@yandex.com>
2016-11-30 19:22:53 +0100

    moved examles to separate module

---
+++ samri/optimization/examples.py
@@ -0,0 +1,17 @@
+import os
+import matplotlib.pyplot as plt
+
+import design
+import registration
+
+def opto_fmri(save_as=False):
+	irf = design.get_irf()
+	print(design.period_padded_period_filtered(irf, 8, 20, 150))
+	if save_as:
+		save_as = os.path.expanduser(save_as)
+		plt.savefig(save_as,dpi=300, transparent=False)
+	else:
+		plt.show()
+
+if __name__ == '__main__':
+	opto_fmri("~/design.png")

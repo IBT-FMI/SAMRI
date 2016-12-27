5e0d68df4fe659057f293e3f305bd39d29f5e865
Horea Christian <horea.christ@yandex.com>
2016-12-27 15:29:13 +0100

    dispatching new command

--- samri/cli.py
+++ samri/cli.py
@@ -2,9 +2,10 @@ __author__ = "Horea Christian"
 
 import argh
 from pipelines.nipype_based.quick import diagnostic
+from pipelines.nipype_based.diagnostics import diagnose
 
 def main():
-	argh.dispatch_commands([diagnostic])
+	argh.dispatch_commands([diagnostic, diagnose])
 
 if __name__ == '__main__':
 	main()

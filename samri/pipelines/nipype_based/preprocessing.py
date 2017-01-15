c7a326a7ede1cbd588f2d0248bd9355af604a149
damaggu <markusm2342@gmail.com>
2017-01-15 18:44:11 +0100

    added crashdump in preprocessing folder

--- samri/pipelines/nipype_based/preprocessing.py
+++ samri/pipelines/nipype_based/preprocessing.py
@@ -366,6 +366,7 @@ def bruker(measurements_base,
 	workflow = pe.Workflow(name=workdir_name)
 	workflow.connect(workflow_connections)
 	workflow.base_dir = path.join(measurements_base,"preprocessing")
+	workflow.config = {"execution": {"crashdump_dir": path.join(measurements_base,"preprocessing/crashdump")}}
 	workflow.write_graph(dotfilename=path.join(workflow.base_dir,workdir_name,"graph.dot"), graph2use="hierarchical", format="png")
 	if not loud:
 		try:

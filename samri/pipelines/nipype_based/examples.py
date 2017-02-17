a8097656a54b310f13927ab14f95273714e1e4ca
Horea Christian <h.chr@mail.ru>
2017-02-17 18:32:13 +0100

    removed redundant turborare name

--- samri/pipelines/nipype_based/examples.py
+++ samri/pipelines/nipype_based/examples.py
@@ -1,6 +1,10 @@
 import preprocessing, glm
 
+def wb_composite():
+	preprocessing.bruker("/home/chymera/NIdata/ofM.dr/",exclude_measurements=['20151027_121613_4013_1_1'], workflow_name="composite", very_nasty_bruker_delay_hack=True, negative_contrast_agent=True, functional_blur_xy=4, functional_registration_method="composite", keep_work=True)
+
 def dr_composite():
+	preprocessing.bruker("/home/chymera/NIdata/ofM.dr/",exclude_measurements=['20151027_121613_4013_1_1'], workflow_name="composite", very_nasty_bruker_delay_hack=True, negative_contrast_agent=True, functional_blur_xy=4, functional_registration_method="composite")
 	glm.l1("~/NIdata/ofM.dr/preprocessing/composite", workflow_name="composite", include={"subjects":[i for i in range(4001,4010)]+[4011,4012]}, habituation="confound",mask="/home/chymera/NIdata/templates/ds_QBI_chr_bin.nii.gz",keep_work=True)
 	glm.l1("~/NIdata/ofM.dr/preprocessing/composite", workflow_name="composite_dr", include={"subjects":[i for i in range(4001,4010)]+[4011,4012]}, habituation="confound",mask="/home/chymera/NIdata/templates/roi/f_dr_chr_bin.nii.gz",)
 	glm.l2_common_effect("~/NIdata/ofM.dr/l1/composite", workflow_name="subjectwise_composite", groupby="subject")
@@ -10,4 +14,5 @@ def vta_composite():
 	preprocessing.bruker("/home/chymera/NIdata/ofM.vta/",workflow_name="composite", very_nasty_bruker_delay_hack=False, negative_contrast_agent=True, functional_blur_xy=4, functional_registration_method="composite")
 
 if __name__ == '__main__':
-	vta_composite()
+	# vta_composite()
+	wb_composite()

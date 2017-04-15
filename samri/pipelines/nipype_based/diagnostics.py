cf157e1de0179abe9e70f524e6ce26c0ec921905
damaggu <markusm2342@gmail.com>
2017-04-15 22:50:25 +0200

    extended registration and realignment logic, FC draft
    * added autorotate as node
    
    * autorotate flag in preprocessing pipeline
    
    * spacetimerealigner as flag, only spm realigner alternatively (works), todo: replace with not spm and add slicetime
    
    * remove spacetime flag from preprocess
    
    * FC version 0.0.0.1 alpha, io with samri not working yet
    
    * PR change requests

--- samri/pipelines/nipype_based/diagnostics.py
+++ samri/pipelines/nipype_based/diagnostics.py
@@ -55,9 +55,9 @@ def diagnose(measurements_base,
 	keep_work=False,
 	loud=False,
 	n_procs=8,
-	realign=False,
+	realign="space",
 	tr=1,
-	workflow_name="diagnostic"
+	workflow_name="diagnostic",
 	):
 
 	measurements_base = path.abspath(path.expanduser(measurements_base))
@@ -149,7 +149,16 @@ def diagnose(measurements_base,
 			(s_bru2nii, datasink, [('nii_file', 'anat')]),
 			])
 
-	if realign:
+	#TODO: case: "space" - melodic ICAs don't break, case: "spacetime" - melodic ICAs break
+	if realign == "space":
+		realigner = pe.Node(interface=spm.Realign(), name="realigner")
+		realigner.inputs.register_to_mean = True
+		workflow_connections.extend([
+			(dummy_scans, realigner, [('out_file', 'in_file')]),
+			(realigner, melodic, [('out_file', 'in_files')]),
+			])
+
+	elif realign == "spacetime"
 		realigner = pe.Node(interface=nipy.SpaceTimeRealigner(), name="realigner")
 		realigner.inputs.slice_times = "asc_alt_2"
 		realigner.inputs.tr = tr
@@ -158,6 +167,7 @@ def diagnose(measurements_base,
 			(dummy_scans, realigner, [('out_file', 'in_file')]),
 			(realigner, melodic, [('out_file', 'in_files')]),
 			])
+
 	else:
 		workflow_connections.extend([
 			(dummy_scans, melodic, [('out_file', 'in_files')]),

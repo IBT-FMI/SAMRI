492cef3fbe5729693602ad5cde1957a94f7936f4
Horea Christian <horea.christ@yandex.com>
2017-04-22 05:39:59 +0200

    updated examples

--- samri/plotting/examples.py
+++ samri/plotting/examples.py
@@ -1,17 +1,18 @@
 import matplotlib.pyplot as plt
 
 from samri.utilities import bids_substitution_iterator
-import maps, timeseries, summary
+import maps, timeseries, summary, network
 
 def overview(workflow, identifiers,
 	cut_coords=[None],
 	threshold=2.5,
+	template="~/ni_data/templates/DSURQEc_40micron_masked.nii.gz",
 	):
 	"""Plot the statistical maps per-factor from a 2nd level GLM workflow result directory."""
 	stat_maps = ["/home/chymera/ni_data/ofM.dr/l2/{0}/{1}/tstat1.nii.gz".format(workflow, i) for i in identifiers]
 	if isinstance(cut_coords[0], int):
 		cut_coords = [cut_coords]
-	maps.stat(stat_maps, template="~/ni_data/templates/ds_QBI_chr.nii.gz", threshold=threshold, interpolation="gaussian", figure_title=workflow, subplot_titles=identifiers, cut_coords=cut_coords)
+	maps.stat(stat_maps, template=template, threshold=threshold, interpolation="gaussian", figure_title=workflow, subplot_titles=identifiers, cut_coords=cut_coords)
 
 def blur_kernel_compare_dr(conditions=["ofM","ofM_aF","ofM_cF1","ofM_cF2","ofM_pF"], parameters=["level2_dgamma","level2_dgamma_blurxy4","level2_dgamma_blurxy5", "level2_dgamma_blurxy6", "level2_dgamma_blurxy7"], threshold=3):
 	from matplotlib.backends.backend_pdf import PdfPages
@@ -28,8 +29,10 @@ def roi_per_session(l1_dir, roi_mask, color,
 	substitutions = bids_substitution_iterator(
 		["ofM","ofM_aF","ofM_cF1","ofM_cF2","ofM_pF"],
 		# ["5689","5690","5691"],
-		["4007","4008","4011","4012","5689","5690","5691"],
-		# ["4011","4012",],
+		["4005","5687","4007","4011","4012","5689","5690","5691"],
+		# ["4007","4011","4012","5689","5690","5691"],
+		# ["4009","4011","4012","5689","5690","5691"],
+		# ["4008","4009","4011","4012",],
 		["EPI_CBV_jb_long","EPI_CBV_chr_longSOA"],
 		"",
 		l1_dir=l1_dir,
@@ -41,7 +44,9 @@ def roi_per_session(l1_dir, roi_mask, color,
 		figure="per-participant",
 		roi_mask=roi_mask,
 		roi_mask_normalize=roi_mask_normalize,
-		color=color)
+		color=color,
+		)
+	print(anova)
 	plt.show()
 
 def p_clusters(mask):
@@ -81,7 +86,7 @@ def roi_teaching(roi_path="~/ni_data/templates/roi/f_dr_chr.nii.gz"):
 def check_responders():
 	summary.responders("subjectwise_composite")
 
-def qc_regressor(mask):
+def qc_regressor_old(mask):
 	substitutions = bids_substitution_iterator(
 		["ofM","ofM_aF","ofM_cF1","ofM_cF2","ofM_pF"],
 		["4011","4012","5689","5690","5691"],
@@ -97,21 +102,39 @@ def qc_regressor(mask):
 	timeseries.multi(timecourses, designs, stat_maps, events_dfs, subplot_titles, figure="timecourses")
 	plt.show()
 
+def qc_regressor(sessions, subjects, scans, workflow_name, mask):
+	substitutions = bids_substitution_iterator(sessions,subjects,scans,workflow_name)
+	timecourses, designs, stat_maps, events_dfs, subplot_titles = summary.ts_overviews(substitutions, mask,
+		ts_file_template="~/ni_data/ofM.dr/preprocessing/{preprocessing_dir}/sub-{subject}/ses-{session}/func/sub-{subject}_ses-{session}_trial-{scan}.nii.gz",
+		beta_file_template="~/ni_data/ofM.dr/l1/{l1_dir}/sub-{subject}/ses-{session}/sub-{subject}_ses-{session}_trial-{scan}_cope.nii.gz",
+		design_file_template="~/ni_data/ofM.dr/l1/{l1_workdir}/_subject_session_scan_{subject}.{session}.{scan}/modelgen/run0.mat",
+		event_file_template="~/ni_data/ofM.dr/preprocessing/{preprocessing_dir}/sub-{subject}/ses-{session}/func/sub-{subject}_ses-{session}_trial-{scan}_events.tsv",
+		)
+	timeseries.multi(timecourses, designs, stat_maps, events_dfs, subplot_titles, figure="timecourses")
+	plt.show()
+
 if __name__ == '__main__':
-	# overview("composite_subjects", ["4007","4008","4011","4012","5689","5690","5691"]) #4001 is a negative control (transgene but no injection
-	# overview("composite_sessions", ["ofM","ofM_aF","ofM_cF1","ofM_cF2","ofM_pF"])
+	# overview("as_composite_subjects", ["4005","4007","4008","4009","4011","4012","5687","5689","5690","5691","5703","5704","5706"],) #4001 is a negative control (transgene but no injection
+	# overview("as_composite_subjects", ["4007","4008","4009","4011","4012","5689","5690","5691"],) #4001 is a negative control (transgene but no injection
+	# overview("as_composite_sessions_responders", ["ofM","ofM_aF","ofM_cF1","ofM_cF2","ofM_pF"],cut_coords=[0,-4.3,-3.3])
+	# overview("as_composite_sessions_best_responders", ["ofM","ofM_aF","ofM_cF1","ofM_cF2","ofM_pF"],cut_coords=[0,-4.3,-3.3])
+	# overview("composite_subjects", ["4007","4008","4011","4012","5689","5690","5691"], template="~/ni_data/templates/ds_QBI_chr.nii.gz") #4001 is a negative control (transgene but no injection
 	# overview("composite_subjects", ["4001","4005","4007","4008","4009","4011","4012"]) #4001 is a negative control (transgene but no injection
 	# overview("subjectwise_blur", ["4001","4005","4007","4008","4009","4011","4012"])
 
-	roi_per_session("composite", "~/ni_data/templates/roi/ctx_chr_bin.nii.gz", "#56B4E9")
-	# roi_per_session("composite", "~/ni_data/templates/roi/ctx_chr_bin.nii.gz", "#56B4E9",
-	# 	roi_mask_normalize="~/ni_data/templates/roi/f_dr_chr_bin.nii.gz",
-	# 	)
-	# roi_per_session("composite", "~/ni_data/templates/roi/f_dr_chr_bin.nii.gz", "#E69F00")
+	# roi_per_session("as_composite", "~/ni_data/templates/roi/DSURQE_ctx.nii.gz", "#e66633")
+	# roi_per_session("as_composite", "~/ni_data/templates/roi/ctx_chr_bin.nii.gz", "#56B4E9")
+	roi_per_session("as_composite", "~/ni_data/templates/roi/DSURQE_ctx.nii.gz", "#56B4E9",
+		roi_mask_normalize="~/ni_data/templates/roi/DSURQE_dr_chr.nii.gz",
+		)
+	# roi_per_session("as_composite", "~/ni_data/templates/roi/DSURQE_dr_chr.nii.gz", "#E69F00")
+	# roi_per_session("as_composite", "~/ni_data/templates/roi/f_dr_chr_bin.nii.gz", "#E69F00")
 	# p_clusters("~/ni_data/templates/roi/f_dr_chr.nii.gz")
 	# p_clusters("~/ni_data/templates/ds_QBI_chr_bin.nii.gz")
 	# roi(roi_path="~/ni_data/templates/roi/f_dr_chr_bin.nii.gz")
 	# roi_teaching()
 	# check_responders()
-	# qc_regressor("~/ni_data/templates/roi/f_dr_chr.nii.gz")
-	# qc_regressor("~/ni_data/templates/roi/ctx_chr.nii.gz")
+	# qc_regressor_old("~/ni_data/templates/roi/f_dr_chr.nii.gz")
+	# qc_regressor_old("~/ni_data/templates/roi/ctx_chr.nii.gz")
+	# qc_regressor(["ofM_cF1"],["4011"],["EPI_CBV_jb_long"],"as_composite","~/ni_data/templates/roi/DSURQE_ctx.nii.gz")
+	# network.simple_dr(output="~/ntw1.png", graphsize=800, scale=1.8)

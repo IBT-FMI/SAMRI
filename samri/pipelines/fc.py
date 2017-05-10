e53de28e7e94af0326390eb64bbb61f89c62891f
Horea Christian <horea.christ@yandex.com>
2017-05-11 01:20:05 +0200

    implemented dual regression up to first level pca

--- samri/pipelines/fc.py
+++ samri/pipelines/fc.py
@@ -1,23 +1,49 @@
-from os import path, listdir, getcwd, remove
+import nibabel
 import nipype.interfaces.io as nio
+from os import path, listdir, getcwd, remove
 
 from nilearn.input_data import NiftiLabelsMasker
 from nilearn.connectome import ConnectivityMeasure
+from nipype.interfaces import fsl
 
 def dual_regression(substitutions_a, substitutions_b,
+	all_merged_path="~/all_merged.nii.gz",
+	components=9,
+	group_level="concat"
+	tr=1,
 	ts_file_template="~/ni_data/ofM.dr/preprocessing/{preprocessing_dir}/sub-{subject}/ses-{session}/func/sub-{subject}_ses-{session}_trial-{scan}.nii.gz",
 	):
 
+	all_merged_path = path.abspath(path.expanduser(all_merged_path))
+
 	ts_a = []
 	for substitution in substitutions_a:
-		ts_a.append(ts_file_template.format(**substitution))
+		ts_a.append(path.abspath(path.expanduser(ts_file_template.format(**substitution))))
 	ts_b = []
 	for substitution in substitutions_b:
-		ts_b.append(ts_file_template.format(**substitution))
+		ts_b.append(path.abspath(path.expanduser(ts_file_template.format(**substitution))))
+
 	ts_all = ts_a + ts_b
-	print(ts_a)
-	print(ts_b)
-	print(ts_all)
+	if group_level == "concat" and not path.isfile(all_merged_path):
+		ts_all_merged = nibabel.concat_images(ts_all, axis=3)
+		ts_all_merged.to_filename(all_merged_path)
+
+	ica = fsl.model.MELODIC()
+	ica.inputs.report = True
+	ica.inputs.tr_sec = tr
+	ica.inputs.no_bet = True
+	ica.inputs.no_mask = True
+	ica.inputs.sep_vn = True
+	if components:
+		ica.inputs.dim = int(components)
+	if group_level == "migp":
+		ica.inputs.in_files = ts_all
+		ica._cmd = 'melodic --migp'
+	elif group_level == concat:
+		ica.inputs.approach = "concat"
+		ica.inputs.in_files = all_merged_path
+	ica_run = ica.run()
+
 
 def correlation_matrix(func_data,
 	mask="/home/chymera/NIdata/templates/ds_QBI_chr_bin.nii.gz",

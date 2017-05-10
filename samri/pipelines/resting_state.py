230fc9b2234bd73b594e6c1cb19b0083e54c91de
Horea Christian <horea.christ@yandex.com>
2017-05-10 21:01:00 +0200

    moved plotting function to correct sub-module

--- samri/pipelines/resting_state.py
+++ samri/pipelines/resting_state.py
@@ -4,29 +4,6 @@ import nipype.interfaces.io as nio
 from nilearn.input_data import NiftiLabelsMasker
 from nilearn.connectome import ConnectivityMeasure
 
-import numpy as np
-
-from matplotlib import pyplot as plt
-
-
-
-def plot_connectivity_matrix(correlation_matrix,
-	figsize,
-	labels,
-	):
-
-	plt.figure(figsize=figsize)
-	np.fill_diagonal(correlation_matrix, 0)
-
-	plt.imshow(correlation_matrix, interpolation="nearest", cmap="RdBu_r",
-			   vmax=0.8, vmin=-0.8)
-
-	x_ticks = plt.xticks(range(len(labels) - 1), labels[1:], rotation=90)
-	y_ticks = plt.yticks(range(len(labels) - 1), labels[1:])
-	plt.gca().yaxis.tick_right()
-	plt.subplots_adjust(left=.01, bottom=.3, top=.99, right=.62)
-
-
 def functional_connectivity(func_data,
 	mask="/home/chymera/NIdata/templates/ds_QBI_chr_bin.nii.gz",
 	labels = '',

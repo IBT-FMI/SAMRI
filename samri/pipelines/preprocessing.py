b1deb717ba40523a0ec5ac78e72eec85cf3149b3
Horea Christian <horea.christ@yandex.com>
2017-05-10 23:02:57 +0200

    absolute import if issues with relative

--- samri/pipelines/preprocessing.py
+++ samri/pipelines/preprocessing.py
@@ -1,7 +1,7 @@
 from os import path, listdir, getcwd, remove
 try:
 	from ..extra_functions import get_data_selection, get_scan, write_events_file, force_dummy_scans
-except ValueError:
+except (SystemError, ValueError):
 	from samri.pipelines.extra_functions import get_data_selection, get_scan, write_events_file, force_dummy_scans
 
 import re

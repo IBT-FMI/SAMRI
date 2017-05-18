236eef83f929be8108d1e1f916ea22d0fe993442
Horea Christian <horea.christ@yandex.com>
2017-05-18 03:07:28 +0200

    added docstring

--- samri/fetch/local.py
+++ samri/fetch/local.py
@@ -4,16 +4,23 @@ import nibabel as nib
 import numpy as np
 import pandas as pd
 
+from scipy import ndimage
+
 def roi_from_atlaslabel(atlas, mapping, label_names,
+	bilateral=True,
+	dilate=True,
 	parser={
 		"textcolumn":"Structure",
 		"valuecolumn right":"right label",
 		"valuecolumn left":"left label",
 		},
-	bilateral=True,
 	save_as="",
 	):
-	"""Return a region of interest map based on an atlas and a label."""
+	"""Return a region of interest map based on an atlas and a label.
+
+	dilate : bool
+	Whether to dilate the roi by one voxel. This is useful for filling up downsampled masks (nearest-neighbour interpolation, may create unexpected holes in masks).
+	"""
 
 	mapping = os.path.abspath(os.path.expanduser(mapping))
 	atlas = os.path.abspath(os.path.expanduser(atlas))
@@ -29,6 +36,8 @@ def roi_from_atlaslabel(atlas, mapping, label_names,
 	affine = atlas.affine
 	data = atlas.get_data()
 	masked_data = np.in1d(data, roi_values).reshape(data.shape).astype(int)
+	if dilate:
+		masked_data = ndimage.binary_dilation(masked_data).astype(masked_data.dtype)
 	roi = nib.Nifti1Image(masked_data, affine, header)
 	if save_as:
 		roi.to_filename(os.path.abspath(os.path.expanduser(save_as)))

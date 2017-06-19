e66d07511167f0248b706e3f599198f4de0d1b2c
Horea Christian <horea.christ@yandex.com>
2017-06-19 10:51:24 +0200

    updated examples

--- samri/plotting/examples.py
+++ samri/plotting/examples.py
@@ -170,13 +170,12 @@ def seed_connectivity_overview(
 		# ["4008","4009","4011","4012",],
 		["EPI_CBV_jb_long","EPI_CBV_chr_longSOA"],
 		"as_composite",
-		l1_dir=l1_dir,
 		)
-	subjectdf, voxeldf = aggregate.fc_rois(substitutions,
-		ts_file_template="~/ni_data/ofM.dr/preprocessing/{preprocessing_dir}/sub-{subject}/ses-{session}/func/sub-{subject}_ses-{session}_trial-{scan}.nii.gz",
-		roi="~/ni_data/templates/roi/DSURQEc_ctx.nii.gz",
-		seed="~/ni_data/templates/roi/DSURQEc_dr.nii.gz",
-		)
+	# subjectdf, voxeldf = aggregate.fc_rois(substitutions,
+		# ts_file_template="~/ni_data/ofM.dr/preprocessing/{preprocessing_dir}/sub-{subject}/ses-{session}/func/sub-{subject}_ses-{session}_trial-{scan}.nii.gz",
+		# roi="~/ni_data/templates/roi/DSURQEc_ctx.nii.gz",
+		# seed="~/ni_data/templates/roi/DSURQEc_dr.nii.gz",
+		# )
 
 	# connectivity_img = fc.seed_based_connectivity(
 	# 	"~/ni_data/ofM.dr/preprocessing/as_composite/sub-5706/ses-ofM_aF/func/sub-5706_ses-ofM_aF_trial-EPI_CBV_chr_longSOA.nii.gz",
@@ -203,8 +202,8 @@ if __name__ == '__main__':
 	# overview("composite_subjects", ["4001","4005","4007","4008","4009","4011","4012"]) #4001 is a negative control (transgene but no injection
 	# overview("subjectwise_blur", ["4001","4005","4007","4008","4009","4011","4012"])
 
-	seed_connectivity_overview()
-	# single_ts_seed_connectivity()
+	# seed_connectivity_overview()
+	single_ts_seed_connectivity()
 
 	# plot_roi_by_label(["medulla","midbrain","pons"],"chr_brainstem")
 	# plot_my_roi()
@@ -231,7 +230,7 @@ if __name__ == '__main__':
 	# 	"~/ni_data/templates/roi/DSURQEc_ctx.nii.gz",
 	# 	)
 	# qc_regressor(["ofM_cF1"],["4011"],["EPI_CBV_jb_long"],"as_composite","~/ni_data/templates/roi/DSURQEc_ctx.nii.gz")
-	# network.simple_dr(output="~/ntw1.png", graphsize=800, scale=1.8)
+	# network.simple_dr(output="/home/chymera/ntw1.png", graphsize=800, scale=1.8)
 
 	# substitutions = bids_substitution_iterator(
 	# 	["ofM","ofM_aF","ofM_cF1","ofM_cF2","ofM_pF"],

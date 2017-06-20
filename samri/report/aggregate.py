149d9a5c96e232d9691ebc7d394fd25131528112
Horea Christian <horea.christ@yandex.com>
2017-06-20 12:07:27 +0200

    trying to set up seconf-level FC analysis

--- samri/report/aggregate.py
+++ samri/report/aggregate.py
@@ -1,14 +1,42 @@
+from nilearn.input_data import NiftiLabelsMasker, NiftiMapsMasker, NiftiMasker
+from samri.utilities import add_roi_data, add_pattern_data
+from samri.pipelines import fc
 
+def add_fc_roi_data(img_path,masker,
+	substitution=False,
+	):
+	"""Return a per-subject and a per-voxel dataframe containing the mean and voxelwise ROI t-scores"""
+	connectivity_img = fc.seed_based_connectivity(img_path, "~/ni_data/templates/roi/DSURQEc_dr.nii.gz",
+		brain_mask="~/ni_data/templates/DSURQEc_200micron_mask.nii.gz",
+	)
+	subject_data={}
+	if substitution:
+		img_path = img_path.format(**substitution)
+	img_path = os.path.abspath(os.path.expanduser(img_path))
+	try:
+		img = nib.load(img_path)
+		img = masker.fit_transform(img)
+		img = img.flatten()
+		mean = np.nanmean(img)
+	except (FileNotFoundError, nib.py3k.FileNotFoundError):
+		return pd.DataFrame({}), pd.DataFrame({})
+	else:
+		subject_data["session"]=substitution["session"]
+		subject_data["subject"]=substitution["subject"]
+		voxel_datas=[]
+		for ix, i in enumerate(img):
+			voxel_data = deepcopy(subject_data)
+			voxel_data["voxel"]=ix
+			voxel_data["t"]=i
+			voxel_data = pd.DataFrame(voxel_data, index=[None])
+			voxel_datas.append(voxel_data)
+		vdf = pd.concat(voxel_datas)
+		subject_data["t"]=mean
+		sdf = pd.DataFrame(subject_data, index=[None])
+		return sdf, vdf
 
-def roi_per_session(substitutions, roi_mask,
-	legend_loc="best",
-	t_file_template="~/ni_data/ofM.dr/l1/{l1_dir}/sub-{subject}/ses-{session}/sub-{subject}_ses-{session}_trial-{scan}_tstat.nii.gz",
-	roi_mask_normalize="",
-	figure="per-participant",
-	tabref="tab",
-	xy_label=[],
-	obfuscate=False,
-	color="#E69F00",
+def seed_fc_rois(substitutions, seed, roi,
+	ts_file_template="~/ni_data/ofM.dr/l1/{l1_dir}/sub-{subject}/ses-{session}/sub-{subject}_ses-{session}_trial-{scan}_tstat.nii.gz",
 	):
 	"""Plot a ROI t-values over the session timecourse
 
@@ -22,14 +50,18 @@ def roi_per_session(substitutions, roi_mask,
 	Path to a ROI mask by the mean of whose t-values to normalite the t-values in roi_mask.
 	"""
 
-	if isinstance(roi_mask,str):
-		roi_mask = os.path.abspath(os.path.expanduser(roi_mask))
-	masker = NiftiMasker(mask_img=roi_mask)
+	if isinstance(roi,str):
+		roi = os.path.abspath(os.path.expanduser(roi_mask))
+	roi = NiftiMasker(mask_img=roi)
+	if isinstance(seed,str):
+		seed = os.path.abspath(os.path.expanduser(roi_mask))
+	seed = NiftiMasker(mask_img=seed)
 
 	n_jobs = mp.cpu_count()-2
-	roi_data = Parallel(n_jobs=n_jobs, verbose=0, backend="threading")(map(delayed(add_roi_data),
-		[t_file_template]*len(substitutions),
-		[masker]*len(substitutions),
+	roi_data = Parallel(n_jobs=n_jobs, verbose=0, backend="threading")(map(delayed(add_fc_roi_data),
+		[ts_file_template]*len(substitutions),
+		[seed]*len(substitutions),
+		[roi]*len(substitutions),
 		substitutions,
 		))
 	subject_dfs, voxel_dfs = zip(*roi_data)

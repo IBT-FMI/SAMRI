f92222895f7a8bc1d70b02256a01797109408f90
damaggu <markusm2342@gmail.com>
2017-07-09 20:19:35 +0200

    internal:added TODO

--- samri/plotting/connectivity.py
+++ samri/plotting/connectivity.py
@@ -24,6 +24,7 @@ def plot_connectivity_matrix(correlation_matrix,
 	"""
 
 	#TODO: fix labels in ascending order corresponding to intensity values
+	#TODO: add example
 	labels = path.abspath(path.expanduser(labels))
 	correlation_matrix = path.abspath(path.expanduser(correlation_matrix))
 

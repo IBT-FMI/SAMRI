3eb8f646113927eefa56c403b1b4192403494b53
Horea Christian <h.chr@mail.ru>
2017-08-17 21:06:18 +0200

    installing package under $HOME

--- installdeps.sh
+++ installdeps.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+echo ""
+echo "======================"
 echo "This is installdeps.sh"
 echo "======================"
 echo ""
@@ -9,22 +11,14 @@ echo "are:"
 ls
 echo ""
 echo "Contents of"
-echo "/usr/portage/packages" 
-echo "are:"
-ls "/usr/portage/packages" 
-echo ""
-echo "Contents of"
-echo "/" 
-echo "are:"
-ls "/" 
-echo ""
-echo "Contents of"
 echo "/home" 
 echo "are:"
 ls "/home" 
 echo ""
-echo "${HOME}"
 
 emerge --sync >> _emerge_sync.txt
+
 echo "======================="
 echo "That was installdeps.sh"
+echo "======================="
+echo ""

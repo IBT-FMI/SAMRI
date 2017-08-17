91154a5d49db1240f4b1051fff75fdaa07d29556
Horea Christian <horea.christ@yandex.com>
2017-08-17 21:34:07 +0200

    Better visual feedback and script structure

--- installdeps.sh
+++ installdeps.sh
@@ -16,9 +16,23 @@ echo "are:"
 ls "/home" 
 echo ""
 
+echo ""
+echo "Preparing Environment:"
+echo "~~~~~~~~~~~~~~~~~~~~~~"
 mkdir .debug
+export FEATURES="-news"
+
+echo ""
+echo "Environment Ready, Emerging:"
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+
 emerge --sync >> .debug/emerge_sync.txt
-emerge wgetpaste
+emerge wgetpaste >> /dev/null
+
+echo ""
+echo "Pastebinning Large Logs:"
+echo "~~~~~~~~~~~~~~~~~~~~~~~~"
+echo "emerge_sync.txt : "
 wgetpaste .debug/emerge_sync.txt
 
 echo ""

40dda1ed0a5a62c82f3d576751ebd353eae534fa
Horea Christian <horea.christ@yandex.com>
2017-08-17 19:50:02 +0200

    diagnostic calls

--- installdeps.sh
+++ installdeps.sh
@@ -1,7 +1,29 @@
 #!/usr/bin/env bash
 
 echo "This is installdeps.sh"
-ls
+echo "======================"
+echo ""
+echo "Contents of"
 pwd
-emerge --sync
+echo "are:"
+ls
+echo ""
+echo "Contents of"
+echo "/usr/portave/packages" 
+echo "are:"
+ls "/usr/portave/packages" 
+echo ""
+echo "Contents of"
+echo "/" 
+echo "are:"
+ls "/" 
+echo ""
+echo "Contents of"
+echo "/home" 
+echo "are:"
+ls "/home" 
+echo ""
+
+emerge --sync >> _emerge_sync.txt
+echo "======================="
 echo "That was installdeps.sh"

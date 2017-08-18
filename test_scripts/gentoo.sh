efbdebfd65b08f388eaacc88d881c71ba9da213a
Horea Christian <horea.christ@yandex.com>
2017-08-18 14:13:34 +0200

    better annotation text

--- test_scripts/gentoo.sh
+++ test_scripts/gentoo.sh
@@ -1,21 +1,9 @@
 #!/usr/bin/env bash
 
 echo ""
-echo "======================"
-echo "This is installdeps.sh"
-echo "======================"
-echo ""
-echo "Contents of"
-pwd
-echo "are:"
-ls
-echo ""
-echo "Contents of"
-echo "/home" 
-echo "are:"
-ls "/home" 
-echo ""
-
+echo "=================="
+echo "Gentoo Test Script"
+echo "=================="
 echo ""
 echo "Preparing Environment:"
 echo "~~~~~~~~~~~~~~~~~~~~~~"
@@ -35,10 +23,3 @@ echo "Pastebinning Large Logs:"
 echo "~~~~~~~~~~~~~~~~~~~~~~~~"
 echo "emerge_sync.txt : "
 wgetpaste .debug/emerge_sync.txt
-
-echo ""
-echo ""
-echo "======================="
-echo "That was installdeps.sh"
-echo "======================="
-echo ""

cfde1f128e52ea44c08a50bdf7f665118ac3c073
Horea Christian <horea.christ@yandex.com>
2017-08-18 14:53:42 +0200

    copying portage files

--- test_scripts/gentoo.sh
+++ test_scripts/gentoo.sh
@@ -10,12 +10,22 @@ echo "Setting up Directory Structure:"
 echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 mkdir .debug
 mkdir /etc/portage/repos.conf
+cat /etc/portage/package.mask
 mkdir /etc/portage/package.mask
 
 echo ""
 echo "Preparing Environment:"
 echo "~~~~~~~~~~~~~~~~~~~~~~"
 export FEATURES="-news"
+cp "test_scripts/gentoo_files/science" "/etc/portage/repos.conf/"
+
+#Link to the workaroud we reproduce in this section : https://wiki.gentoo.org/wiki/User_talk:Houseofsuns#Migration_to_science_overlay_from_main_tree
+#Efforts to more permanently address the issue: https://github.com/gentoo/sci/issues/805
+echo ""
+echo "Setting Up Eselect for Gentoo Science:"
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+cp "${HOME}/SAMRI/test_scripts/gentoo_files/sci-lapack" "/etc/portage/package.mask/"
+
 
 echo ""
 echo "Environment Ready, Emerging:"

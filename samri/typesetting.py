d16f5337529b7257c57d235ca8969bec84c4d027
Horea Christian <horea.christ@yandex.com>
2017-09-07 20:39:13 +0200

    fixed indent and parameter passing

--- samri/typesetting.py
+++ samri/typesetting.py
@@ -22,38 +22,39 @@ def float_to_tex(f,
 	return f_str
 
 def inline_anova(df, factor,
-        style="python",
+	style="python",
+	max_len=4,
         ):
-        """Typeset factor summary from statsmodels-style anova DataFrame for inline mention.
+	"""Typeset factor summary from statsmodels-style anova DataFrame for inline mention.
 
-        Parameters
-        ----------
-        df : pandas.DataFrame
-                Pandas DataFrame object containing an ANOVA summary.
-        factor : str
-                String indicating the factor of interest from the summary given by `df`.
-        style : {"python", "tex"}
-                What formatting to apply to the string. A simple Python compatible string is returned when selecting "python", whereas a fancier output (decorated with TeX syntax) is returned if selecting "tex".
-        """
+	Parameters
+	----------
+	df : pandas.DataFrame
+		Pandas DataFrame object containing an ANOVA summary.
+	factor : str
+		String indicating the factor of interest from the summary given by `df`.
+	style : {"python", "tex"}
+		What formatting to apply to the string. A simple Python compatible string is returned when selecting "python", whereas a fancier output (decorated with TeX syntax) is returned if selecting "tex".
+	"""
 
-        if style == "python":
+	if style == "python":
 		inline = "F({:g},{:g})={:2G}, p={:3G}".format(
-                        df["df"][factor],
-                        df["df"]["Residual"],
-                        df["F"][factor],
-                        df["PR(>F)"][factor],
-                        )
-        elif style == "tex":
-		degrees_of_freedom = float_to_tex(df["df"][factor])
-		degrees_of_freedom_rest = float_to_tex(df["df"]["Residual"])
-		f_string = float_to_tex(df["F"][factor])
-		p_string = float_to_tex(df["PR(>F)"][factor])
-                inline = "$F_{{{},{}}}={},\\, p={}$".format(
-                        degrees_of_freedom,
-                        degrees_of_freedom_rest,
-                        f_string,
-                        p_string,
-                        )
+			df["df"][factor],
+			df["df"]["Residual"],
+			df["F"][factor],
+			df["PR(>F)"][factor],
+			)
+	elif style == "tex":
+		degrees_of_freedom = float_to_tex(df["df"][factor], max_len=max_len)
+		degrees_of_freedom_rest = float_to_tex(df["df"]["Residual"], max_len=max_len)
+		f_string = float_to_tex(df["F"][factor], max_len=max_len)
+		p_string = float_to_tex(df["PR(>F)"][factor], max_len=max_len)
+		inline = "$F_{{{},{}}}={},\\, p={}$".format(
+			degrees_of_freedom,
+			degrees_of_freedom_rest,
+			f_string,
+			p_string,
+			)
 
-        return inline
+	return inline
 

730f0b0d43f032d3fa64c1e8fd9ea51910ab59f5
Horea Christian <horea.christ@yandex.com>
2017-09-07 20:15:46 +0200

    fixed indents

--- samri/examples/registration_qc.py
+++ samri/examples/registration_qc.py
@@ -7,8 +7,8 @@ data_dir = path.join(path.dirname(path.realpath(__file__)),"../../example_data/"
 df_path = path.join(data_dir,"f_reg_quality.csv")
 anova_summary = registration_qc(df_path,
 	value={"similarity":"Similarity"},
-        group={"sub":"Subject"},
-        repeat={"ses":"Session"},
+	group={"sub":"Subject"},
+	repeat={"ses":"Session"},
 	show=False,
 	save_as="registration_qc.png",
 	)

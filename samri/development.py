f5a0fd159aee9bac206cd7d8170ace8af3ab95a2
Horea Christian <horea.christ@yandex.com>
2017-09-08 20:50:40 +0200

    Demonstrating parameterized qc function and saving .csv

--- samri/development.py
+++ samri/development.py
@@ -27,34 +27,6 @@ def reg_gc():
 	session_effect = inline_anova(anova_summary,"C(Session)",style="python")
 	print("Session Main Effect: {}".format(session_effect))
 
-def reg_cc3():
-	from samri.plotting.aggregate import registration_qc
-	from samri.report.registration import get_scores
-	from samri.typesetting import inline_anova
-
-	df = get_scores(
-		"~/ni_data/ofM.dr/preprocessing/composite",
-		"~/ni_data/templates/DSURQEc_200micron_average.nii",
-		metric="CC",
-		radius_or_number_of_bins = 3,
-		sampling_strategy = "Regular",
-		sampling_percentage=0.33,
-		save_as=False,
-		)
-
-	anova_summary = registration_qc(df,
-		value={"similarity":"Similarity"},
-		group={"sub":"Subject"},
-		repeat={"ses":"Session"},
-		show=False,
-		save_as="/tmp/f_reg_cc3.png",
-		)
-
-	subject_effect = inline_anova(anova_summary,"C(Subject)",style="python")
-	print("Subject Main Effect: {}".format(subject_effect))
-	session_effect = inline_anova(anova_summary,"C(Session)",style="python")
-	print("Session Main Effect: {}".format(session_effect))
-
 def reg_cc10():
 	from samri.plotting.aggregate import registration_qc
 	from samri.report.registration import get_scores
@@ -76,21 +48,27 @@ def reg_cc10():
 		radius_or_number_of_bins=10,
 		sampling_strategy="Regular",
 		sampling_percentage=0.33,
-		save_as=False,
+		save_as="f_reg_quality.csv",
 		)
 
-	anova_summary = registration_qc(df,
+	anova_summary = registration_qc(df_path,
 		value={"similarity":"Similarity"},
 		group={"sub":"Subject"},
 		repeat={"ses":"Session"},
+		extra={"trial":"Type"},
+		model="{value} ~ C({extra}) + C({repeat}) + C({group}) -1",
+		save_as="registration_qc.png",
+		print_model=True,
+		print_anova=True,
 		show=False,
-		save_as="/tmp/f_reg_cc10.png",
 		)
 
 	subject_effect = inline_anova(anova_summary,"C(Subject)",style="python")
 	print("Subject Main Effect: {}".format(subject_effect))
-	session_effect = inline_anova(anova_summary,"C(Session)",style="python")
+	session_effect = inline_anova(anova_summary,"C(Session)",style="python", max_len=2)
 	print("Session Main Effect: {}".format(session_effect))
+	type_effect = inline_anova(anova_summary,"C(Type)",style="python")
+	print("Scan Type Main Effect: {}".format(type_effect))
 
 def test_autofind():
 	"""We may be able to turn this into a CI function, if we put together a data fetching script for dummy (empty) BIDS-formatted data.

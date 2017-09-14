3a0b18ec47caa105759888f2b3bfe442709b592e
damaggu <marksm@ethz.ch>
2017-09-14 19:22:50 +0200

    fixed indents

--- samri/fetch/templates.py
+++ samri/fetch/templates.py
@@ -8,7 +8,7 @@ def fetch_rat_waxholm(template_dir="~/.samri_files/templates/rat/waxholm/", verb
 	"""Download and load waxholm atlas for Sprague Dawley rat
 
 	Returns:
-        data:   sklearn.datasets.base.Bunch Dictionary-like object
+	data:   sklearn.datasets.base.Bunch Dictionary-like object
 		template
 		atlas
 		labels
@@ -21,7 +21,7 @@ def fetch_rat_waxholm(template_dir="~/.samri_files/templates/rat/waxholm/", verb
 	https://www.nitrc.org/projects/whs-sd-atlas
 
 	"""
-        # Get template
+	# Get template
 	url_template = 'https://www.nitrc.org/frs/download.php/9423/WHS_SD_rat_T2star_v1.01.nii.gz'
 	template = _fetch_files(path.abspath(path.expanduser(template_dir)), [('WHS_SD_rat_T2star_v1.01.nii.gz', url_template, {})],
 			verbose=verbose)[0]
@@ -35,28 +35,28 @@ def fetch_rat_waxholm(template_dir="~/.samri_files/templates/rat/waxholm/", verb
 	url_labels = 'https://www.nitrc.org/frs/download.php/9439/WHS_SD_rat_atlas_v2.label'
 	labels = _fetch_files(path.abspath(path.expanduser(template_dir)), [('WHS_SD_rat_atlas_v2.label', url_labels, {})],
 			verbose=verbose)[0]
-        
+
 	return Bunch(
 			template=template,
 			atlas=atlas,
 			labels=labels)
+	
+	
+def fetch_mouse_DSURQE(template_dir="~/.samri_files/templates/mouse/DSURQE/", verbose=1):
+	"""Download and load waxholm atlas for Sprague Dawley rat
 
-
-def fetch_mouse_DSURQE(template_dir="~/.samri_files/templates/mouse/DSURQE/", verbose=1):                                   
-        """Download and load waxholm atlas for Sprague Dawley rat
-
-        Returns:
-        data:   sklearn.datasets.base.Bunch Dictionary-like object
+	Returns:
+	data:   sklearn.datasets.base.Bunch Dictionary-like object
 		template
 		atlas
 		mask
 		labels
 
-        References:
-        'A.E. Dorr, et al, High resolution three-dimensional brain atlas using an average magnetic resonance image of 40 adult C57Bl/6J mice, NeuroImage, vol 42, Aug. 2008, pp. 60-69'
+	References:
+	'A.E. Dorr, et al, High resolution three-dimensional brain atlas using an average magnetic resonance image of 40 adult C57Bl/6J mice, NeuroImage, vol 42, Aug. 2008, pp. 60-69'
 
-        more information:
-        https://wiki.mouseimaging.ca/display/MICePub/Mouse+Brain+Atlases
+	more information:
+	https://wiki.mouseimaging.ca/display/MICePub/Mouse+Brain+Atlases
 
 	"""
 
@@ -72,9 +72,9 @@ def fetch_mouse_DSURQE(template_dir="~/.samri_files/templates/mouse/DSURQE/", ve
 
 	# Get mask
 
-        url_mask = 'http://repo.mouseimaging.ca/repo/DSURQE_40micron_nifti/DSURQE_40micron_mask.nii'
-        mask = _fetch_files(path.abspath(path.expanduser(template_dir)), [('DSURQE_40micron_mask.nii', url_mask, {})],
-                        verbose=verbose)[0]
+	url_mask = 'http://repo.mouseimaging.ca/repo/DSURQE_40micron_nifti/DSURQE_40micron_mask.nii'
+	mask = _fetch_files(path.abspath(path.expanduser(template_dir)), [('DSURQE_40micron_mask.nii', url_mask, {})],
+			verbose=verbose)[0]
 
 	# Get labels
 	url_labels = 'http://repo.mouseimaging.ca/repo/DSURQE_40micron/DSURQE_40micron_itksnap_mapping.txt'
@@ -86,17 +86,17 @@ def fetch_mouse_DSURQE(template_dir="~/.samri_files/templates/mouse/DSURQE/", ve
 		"fslorient -copysform2qform DSURQE_40micron_average.nii",
 		"mv DSURQE_40micron_average.nii DSURQEc_40micron_average.nii",
 		"fslorient -setsform -0.04 0 0 6.27 0 0.04 0 -10.7 0 0 0.04 -7.9 0 0 0 1 DSURQE_40micron_mask.nii",
-                "fslorient -copysform2qform DSURQE_40micron_mask.nii",
-                "mv DSURQE_40micron_mask.nii DSURQEc_40micron_mask.nii",
+		"fslorient -copysform2qform DSURQE_40micron_mask.nii",
+		"mv DSURQE_40micron_mask.nii DSURQEc_40micron_mask.nii",
 		"fslorient -setsform -0.04 0 0 6.27 0 0.04 0 -10.7 0 0 0.04 -7.9 0 0 0 1 DSURQE_40micron_labels.nii",
-                "fslorient -copysform2qform DSURQE_40micron_labels.nii",
-                "mv DSURQE_40micron_labels.nii DSURQEc_40micron_labels.nii",
+		"fslorient -copysform2qform DSURQE_40micron_labels.nii",
+		"mv DSURQE_40micron_labels.nii DSURQEc_40micron_labels.nii",
 		"ResampleImage 3 DSURQEc_40micron_average.nii _DSURQEc_200micron_average.nii 0.2x0.2x0.2 size=1 spacing=0 4",	
 		"SmoothImage 3 _DSURQEc_200micron_average.nii 0.4 DSURQEc_200micron_average.nii",
 		"rm _DSURQEc_200micron_average.nii",
-                "ResampleImage 3 DSURQEc_40micron_labels.nii _DSURQEc_200micron_labels.nii 0.2x0.2x0.2 size=1 spacing=0 4",
-                "SmoothImage 3 _DSURQEc_200micron_labels.nii 0.4 DSURQEc_200micron_labels.nii",
-                "rm _DSURQEc_200micron_labels.nii",
+		"ResampleImage 3 DSURQEc_40micron_labels.nii _DSURQEc_200micron_labels.nii 0.2x0.2x0.2 size=1 spacing=0 4",
+		"SmoothImage 3 _DSURQEc_200micron_labels.nii 0.4 DSURQEc_200micron_labels.nii",
+		"rm _DSURQEc_200micron_labels.nii",
 		"ResampleImage 3 DSURQEc_40micron_mask.nii DSURQEc_200micron_mask.nii 0.2x0.2x0.2 size=1 spacing=0 1",
 		"mv DSURQE_40micron_itksnap_mapping.txt DSURQEc_40micron_itksnap_mapping.txt"]	
 	
@@ -104,8 +104,8 @@ def fetch_mouse_DSURQE(template_dir="~/.samri_files/templates/mouse/DSURQE/", ve
 		p = subprocess.Popen(command.split(), cwd=path.abspath(path.expanduser(template_dir)), stdout=subprocess.PIPE)
 		p.wait()
 
-        return Bunch(                                      
-                        template=path.abspath(path.expanduser(template_dir)) + "DSURQEc_40micron_average.nii",                 
-                        atlas=path.abspath(path.expanduser(template_dir)) + "DSURQEc_40micron_labels.nii",
-			mask=path.abspath(path.expanduser(template_dir)) + "DSURQEc_40micron_mask.nii",                       
-                        labels=labels)
+	return Bunch(
+			template=path.abspath(path.expanduser(template_dir)) + "DSURQEc_40micron_average.nii",
+			atlas=path.abspath(path.expanduser(template_dir)) + "DSURQEc_40micron_labels.nii",
+			mask=path.abspath(path.expanduser(template_dir)) + "DSURQEc_40micron_mask.nii",
+			labels=labels)

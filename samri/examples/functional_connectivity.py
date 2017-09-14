3e437eaffd6380708fdbf7066ce992ab74ab8fc7
damaggu <marksm@ethz.ch>
2017-09-14 21:36:25 +0200

    fc example, still to test

---
+++ samri/examples/functional_connectivity.py
@@ -0,0 +1,17 @@
+# This example illustrates how to generate a functional connectivity matrix and it's respective plot
+
+from os import path
+from samri.pipelines import fc
+from samri.plotting import connectivity
+from samri.fetch import fetch_mouse_DSURQE
+
+# fetch data templates and data
+data_dir = path.join(path.dirname(path.realpath(__file__)),"../../example_data/")
+results_dir = path.abspath(path.expanduser('~/.samri_files/results/')
+template = fetch_mouse_DSURQE()
+
+# run analysis and plot result
+ts= data_dir + "sub-5690_ses-ofM_aF_trial-EPI_CBV_chr_longSOA.nii.gz"
+figsize=(50,50)
+correlation_matrix = fc.correlation_matrix(ts, template.labels, save_as = results_dir + '/fc/correlation_matrix.csv')
+connectivity.plot_connectivity_matrix(correlation_matrix, figsize, template.labels, save_as = results_dir + '/fc/correlation_matrix.png')

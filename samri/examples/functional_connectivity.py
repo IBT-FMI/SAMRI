72bf3cc41fbfd0c654c47e5213a19e1552083d29
damaggu <marksm@ethz.ch>
2017-09-21 21:41:13 +0200

    fixed import, check for folder existence, use example_data timeseries

--- samri/examples/functional_connectivity.py
+++ samri/examples/functional_connectivity.py
@@ -1,18 +1,22 @@
 # This example illustrates how to generate a functional connectivity matrix and it's respective plot
 
+import os
 from os import path
 from samri.pipelines import fc
 from samri.plotting import connectivity
-from samri.fetch import fetch_mouse_DSURQE
+from samri.fetch.templates import fetch_mouse_DSURQE
 
 # fetch data templates and data
 data_dir = path.join(path.dirname(path.realpath(__file__)),"../../example_data/")
-results_dir = path.abspath(path.expanduser('~/.samri_files/results/'))
+results_dir = path.abspath(path.expanduser('~/.samri_files/results/fc/'))
+# check if results dir exists, otherwise create
+if not os.path.exists(path.abspath(path.expanduser(results_dir))):	
+	os.makedirs(path.abspath(path.expanduser(results_dir)))
+
 template = fetch_mouse_DSURQE()
 
 # run analysis and plot result
-#ts= data_dir + "sub-5690_ses-ofM_aF_trial-EPI_CBV_chr_longSOA.nii.gz"
-ts= "~/ni_data/ofM.dr/preprocessing/marksm_composite/sub-5690/ses-ofM_aF/sub-5690_ses-ofM_aF_trial-EPI_CBV_chr_longSOA.nii.gz"
+ts= data_dir + "sub-5690_ses-ofM_aF_trial-EPI_CBV_chr_longSOA.nii.gz"
 figsize=(50,50)
-correlation_matrix = fc.correlation_matrix(ts, template.labels, save_as = results_dir + '/fc/correlation_matrix.csv')
-connectivity.plot_connectivity_matrix(correlation_matrix, figsize, template.labels, save_as = results_dir + '/fc/correlation_matrix.png')
+correlation_matrix = fc.correlation_matrix(ts, labels_img = template['atlas'], save_as = results_dir + '/correlation_matrix.csv')
+connectivity.plot_connectivity_matrix(correlation_matrix, figsize = figsize, labels=template['labels'], save_as = results_dir + '/correlation_matrix.png')

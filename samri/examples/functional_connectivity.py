b63c2622adc1d13eb6e3e6b36c91a2951c7ebdb2
damaggu <marksm@ethz.ch>
2017-09-21 20:04:06 +0200

    temp

--- samri/examples/functional_connectivity.py
+++ samri/examples/functional_connectivity.py
@@ -7,11 +7,12 @@ from samri.fetch import fetch_mouse_DSURQE
 
 # fetch data templates and data
 data_dir = path.join(path.dirname(path.realpath(__file__)),"../../example_data/")
-results_dir = path.abspath(path.expanduser('~/.samri_files/results/')
+results_dir = path.abspath(path.expanduser('~/.samri_files/results/'))
 template = fetch_mouse_DSURQE()
 
 # run analysis and plot result
-ts= data_dir + "sub-5690_ses-ofM_aF_trial-EPI_CBV_chr_longSOA.nii.gz"
+#ts= data_dir + "sub-5690_ses-ofM_aF_trial-EPI_CBV_chr_longSOA.nii.gz"
+ts= "~/ni_data/ofM.dr/preprocessing/marksm_composite/sub-5690/ses-ofM_aF/sub-5690_ses-ofM_aF_trial-EPI_CBV_chr_longSOA.nii.gz"
 figsize=(50,50)
 correlation_matrix = fc.correlation_matrix(ts, template.labels, save_as = results_dir + '/fc/correlation_matrix.csv')
 connectivity.plot_connectivity_matrix(correlation_matrix, figsize, template.labels, save_as = results_dir + '/fc/correlation_matrix.png')

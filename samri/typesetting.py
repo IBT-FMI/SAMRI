416c543d63831c8624a357cd6214c3d777fac1f8
Horea Christian <horea.christ@yandex.com>
2017-10-01 22:38:03 +0200

    Added condensed mode

--- samri/typesetting.py
+++ samri/typesetting.py
@@ -1,5 +1,6 @@
 def float_to_tex(f,
 	max_len=4,
+	condensed=False,
 	):
 	"""Reformat float to tex syntax
 
@@ -12,6 +13,12 @@ def float_to_tex(f,
 		Maximum digit length of output strings.
 	"""
 
+	if condensed:
+		model_str="{}\\!\\!\\times\\!\\!10^{{{}}}"
+	else:
+		model_str="{} \\times 10^{{{}}}"
+
+
 	if f >= 10**-max_len and f <= 10**max_len:
 		f_str_template = "{{:.{}g}}".format(max_len)
 		f_str = f_str_template.format(f)
@@ -19,14 +26,15 @@ def float_to_tex(f,
 		f_str = "{:e}".format(f)
 		f_decimals, f_exponent = f_str.split("e")
 		truncated_decimals = f_decimals[:max_len].rstrip('.')
-		f_str = "{} \\times 10^{{{}}}".format(truncated_decimals,int(f_exponent))
+		f_str = model_str.format(truncated_decimals,int(f_exponent))
 
 	return f_str
 
 def inline_anova(df, factor,
 	style="python",
 	max_len=4,
-        ):
+	condensed=False,
+	):
 	"""Typeset factor summary from statsmodels-style anova DataFrame for inline mention.
 
 	Parameters
@@ -49,14 +57,18 @@ def inline_anova(df, factor,
 	elif style == "tex":
 		degrees_of_freedom = float_to_tex(df["df"][factor], max_len=max_len)
 		degrees_of_freedom_rest = float_to_tex(df["df"]["Residual"], max_len=max_len)
-		f_string = float_to_tex(df["F"][factor], max_len=max_len)
-		p_string = float_to_tex(df["PR(>F)"][factor], max_len=max_len)
+		f_string = float_to_tex(df["F"][factor], max_len=max_len, condensed=condensed)
+		p_string = float_to_tex(df["PR(>F)"][factor], max_len=max_len, condensed=condensed)
 		inline = "$F_{{{},{}}}={},\\, p={}$".format(
 			degrees_of_freedom,
 			degrees_of_freedom_rest,
 			f_string,
 			p_string,
 			)
+		if condensed:
+			header='\\begingroup \\setlength{\\thickmuskip}{0mu}'
+			footer = '\\endgroup'
+			inline = header + inline + footer
 
 	return inline
 

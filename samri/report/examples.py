b3f00c0e6f1a1b330fda39afc93d9c70c05396d6
Horea Christian <horea.christ@yandex.com>
2017-11-13 19:39:16 +0100

    Using absolute import

--- samri/report/examples.py
+++ samri/report/examples.py
@@ -1,5 +1,4 @@
-from roi import roi_mean
-
+from samri.report.roi import mean
 
 if __name__ == '__main__':
 	roi_mean("~/Downloads/Christian/10_575536_1_EV1.nii", "~/Downloads/Christian/10_575536_1_brainstem.roi")

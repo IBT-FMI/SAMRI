1753c85cec6a86d9e93f402906c3b5f071a13a11
Horea Christian <horea.christ@yandex.com>
2017-12-01 00:48:23 +0100

    Updated example

--- samri/examples/registration_qc.py
+++ samri/examples/registration_qc.py
@@ -1,23 +1,31 @@
+import pandas as pd
+import statsmodels.api as sm
+import statsmodels.formula.api as smf
 from os import path
 from samri.plotting.aggregate import registration_qc
 from samri.typesetting import inline_anova
 
-
 data_dir = path.join(path.dirname(path.realpath(__file__)),"../../example_data/")
 df_path = path.join(data_dir,"f_reg_quality.csv")
 
-anova_summary = registration_qc(df_path,
+registration_qc(df_path,
 	value={"similarity":"Similarity"},
-	group={"sub":"Subject"},
-	repeat={"ses":"Session"},
-	extra={"trial":"Type"},
+	group={"subject":"Subject"},
+	repeat={"session":"Session"},
+	extra={"acquisition":"Acquisition"},
 	save_as="registration_qc.png",
 	show=False,
 	)
 
-print(inline_anova(anova_summary,"C(Subject)",style="python"))
-print(inline_anova(anova_summary,"C(Session)",style="python", max_len=2))
-print(inline_anova(anova_summary,"C(Type)",style="python"))
-print(inline_anova(anova_summary,"C(Subject)",style="tex"))
-print(inline_anova(anova_summary,"C(Session)",style="tex", max_len=2))
-print(inline_anova(anova_summary,"C(Type)",style="tex"))
+df = pd.read_csv(df_path)
+#model="similarity ~ C(acquisition) + C(session) + C(subject)"
+model="similarity ~ C(session) + C(subject)"
+regression_model = smf.ols(model, data=df).fit()
+anova_summary = sm.stats.anova_lm(regression_model, typ=2)
+
+print(inline_anova(anova_summary,"C(subject)",style="python"))
+print(inline_anova(anova_summary,"C(session)",style="python", max_len=2))
+#print(inline_anova(anova_summary,"C(acquisition)",style="python"))
+print(inline_anova(anova_summary,"C(subject)",style="tex"))
+print(inline_anova(anova_summary,"C(session)",style="tex", max_len=2))
+#print(inline_anova(anova_summary,"C(acquisition)",style="tex"))

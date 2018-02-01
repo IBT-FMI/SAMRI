188fadf621a93b0325b987b9b54e1c0bfc012c3c
damaggu <marksm@ethz.ch>
2018-02-01 20:41:58 +0100

    preprocessing to bids compliant bids preprocessing

--- samri/pipelines/preprocess.py
+++ samri/pipelines/preprocess.py
@@ -1,5 +1,5 @@
 from os import path, listdir, getcwd, remove
-from samri.pipelines.extra_functions import get_data_selection, get_scan, write_bids_metadata_file, write_events_file, force_dummy_scans, BIDS_METADATA_EXTRACTION_DICTS
+from samri.pipelines.extra_functions import get_data_selection, get_scan, get_bids_scan, write_bids_metadata_file, write_events_file, force_dummy_scans, BIDS_METADATA_EXTRACTION_DICTS
 
 import re
 import inspect
@@ -19,14 +19,36 @@ from samri.pipelines.utils import bids_naming, ss_to_path, sss_filename, fslmath
 from samri.utilities import N_PROCS
 from samri.fetch.templates import fetch_rat_waxholm, fetch_mouse_DSURQE
 
+from bids.grabbids import BIDSLayout
+
 DUMMY_SCANS=10
 N_PROCS=max(N_PROCS-4, 2)
 
+
+debug = True
 #set all outputs to compressed NIfTI
 afni.base.AFNICommand.set_default_output_type('NIFTI_GZ')
 fsl.FSLCommand.set_default_output_type('NIFTI_GZ')
 
-def bruker(measurements_base, template,
+
+def bidsDataSelection(base,
+		):
+	layout = BIDSLayout(base)
+	df = layout.as_data_frame()
+	# drop event files
+	df = df[df.type != 'events']
+	# rm .json
+	df = df.loc[df.path.str.contains('.nii')]
+	#df = df.loc[df.path.str.contains('.json')]
+	df['trial'] = df['type']
+	# generate scan types for later
+	df['scan_type'] = ""
+	df.loc[df.modality == 'func', 'scan_type'] = 'acq-'+df['acq']+'_trial-'+df['trial']
+	df.loc[df.modality == 'anat', 'scan_type'] = 'acq-'+df['acq']+'_' + df['type']
+	
+	return df
+
+def bruker(bids_base, template,
 	DEBUG=False,
 	exclude={},
 	functional_match={},
@@ -62,14 +84,13 @@ def bruker(measurements_base, template,
 			registration_mask = fetch_mouse_DSURQE()['mask']
 		elif template == "rat":
 			template = fetch_rat_waxholm()['template']
-			registration_mask = fetch_rat_waxholm()['mask']
 		else:
 			pass
 	else:
 		raise ValueError("No species or template specified")
 		return -1
 
-	measurements_base = path.abspath(path.expanduser(measurements_base))
+	bids_base = path.abspath(path.expanduser(bids_base))
 
 	# add subject and session filters if present
 	if subjects:
@@ -77,30 +98,16 @@ def bruker(measurements_base, template,
 	if sessions:
 		structural_scan_types['session'] = sessions
 
-	# define measurement directories to be processed, and populate the list either with the given include_measurements, or with an intelligent selection
-	data_selection = pd.DataFrame([])
-	if structural_match:
-		s_data_selection = get_data_selection(measurements_base,
-			match=structural_match,
-			exclude=exclude,
-			)
-		structural_scan_types = s_data_selection['scan_type'].unique()
-		data_selection = pd.concat([data_selection,s_data_selection])
-	if functional_match:
-		f_data_selection = get_data_selection(measurements_base,
-			match=functional_match,
-			exclude=exclude,
-			)
-		functional_scan_types = f_data_selection['scan_type'].unique()
-		data_selection = pd.concat([data_selection,f_data_selection])
 
-	# we currently only support one structural scan type per session
-	#if functional_registration_method in ("structural", "composite") and structural_scan_types:
-	#	structural_scan_types = [structural_scan_types[0]]
+	data_selection = bidsDataSelection(bids_base)
+	
+	# generate functional and structural scan types
+	functional_scan_types = data_selection.loc[data_selection.modality == 'func']['scan_type'].values
+	structural_scan_types = data_selection.loc[data_selection.modality == 'anat']['scan_type'].values
 
 	# we start to define nipype workflow elements (nodes, connections, meta)
 	subjects_sessions = data_selection[["subject","session"]].drop_duplicates().values.tolist()
-	if debug:
+	if True:
 		print('Data selection:')
 		print(data_selection)
 		print('Iterating over:')
@@ -108,15 +115,12 @@ def bruker(measurements_base, template,
 	infosource = pe.Node(interface=util.IdentityInterface(fields=['subject_session'], mandatory_inputs=False), name="infosource")
 	infosource.iterables = [('subject_session', subjects_sessions)]
 
-	get_f_scan = pe.Node(name='get_f_scan', interface=util.Function(function=get_scan,input_names=inspect.getargspec(get_scan)[0], output_names=['scan_path','scan_type','trial']))
+	get_f_scan = pe.Node(name='get_f_scan', interface=util.Function(function=get_bids_scan,input_names=inspect.getargspec(get_bids_scan)[0], output_names=['scan_path','scan_type','trial', 'nii_path']))
 	if not strict:
 		get_f_scan.inputs.ignore_exception = True
 	get_f_scan.inputs.data_selection = data_selection
-	get_f_scan.inputs.measurements_base = measurements_base
+	get_f_scan.inputs.bids_base = bids_base
 	get_f_scan.iterables = ("scan_type", functional_scan_types)
-
-	f_bru2nii = pe.Node(interface=bru2nii.Bru2(), name="f_bru2nii")
-	f_bru2nii.inputs.actual_size=actual_size
 
 	dummy_scans = pe.Node(name='dummy_scans', interface=util.Function(function=force_dummy_scans,input_names=inspect.getargspec(force_dummy_scans)[0], output_names=['out_file']))
 	dummy_scans.inputs.desired_dummy_scans = DUMMY_SCANS
@@ -140,13 +144,15 @@ def bruker(measurements_base, template,
 
 	events_file = pe.Node(name='events_file', interface=util.Function(function=write_events_file,input_names=inspect.getargspec(write_events_file)[0], output_names=['out_file']))
 	events_file.inputs.dummy_scans_ms = DUMMY_SCANS * tr * 1000
-	events_file.inputs.stim_protocol_dictionary = STIM_PROTOCOL_DICTIONARY
+	#TODO: was tun mit dem hier?
+	#events_file.inputs.stim_protocol_dictionary = STIM_PROTOCOL_DICTIONARY
+	
 	events_file.inputs.very_nasty_bruker_delay_hack = very_nasty_bruker_delay_hack
 	if not (strict or verbose):
 		events_file.inputs.ignore_exception = True
 
 	datasink = pe.Node(nio.DataSink(), name='datasink')
-	datasink.inputs.base_directory = path.join(measurements_base,"preprocessing",workflow_name)
+	datasink.inputs.base_directory = path.join(bids_base,"preprocessing",workflow_name)
 	datasink.inputs.parameterization = False
 	if not (strict or verbose):
 		datasink.inputs.ignore_exception = True
@@ -155,8 +161,7 @@ def bruker(measurements_base, template,
 		(infosource, get_f_scan, [('subject_session', 'selector')]),
 		(infosource, bids_stim_filename, [('subject_session', 'subject_session')]),
 		(get_f_scan, bids_stim_filename, [('scan_type', 'scan_type')]),
-		(get_f_scan, f_bru2nii, [('scan_path', 'input_dir')]),
-		(f_bru2nii, dummy_scans, [('nii_file', 'in_file')]),
+		(get_f_scan, dummy_scans, [('nii_path', 'in_file')]),
 		(get_f_scan, dummy_scans, [('scan_path', 'scan_dir')]),
 		(get_f_scan, events_file, [
 			('trial', 'trial'),
@@ -201,16 +206,13 @@ def bruker(measurements_base, template,
 		s_biascorrect, f_biascorrect = inflated_size_nodes()
 
 	if structural_scan_types.any():
-		get_s_scan = pe.Node(name='get_s_scan', interface=util.Function(function=get_scan, input_names=inspect.getargspec(get_scan)[0], output_names=['scan_path','scan_type','trial']))
+		get_s_scan = pe.Node(name='get_s_scan', interface=util.Function(function=get_bids_scan, input_names=inspect.getargspec(get_bids_scan)[0], output_names=['scan_path','scan_type',
+			'trial', 'nii_path']))
 		if not strict:
 			get_s_scan.inputs.ignore_exception = True
 		get_s_scan.inputs.data_selection = data_selection
-		get_s_scan.inputs.measurements_base = measurements_base
+		get_s_scan.inputs.bids_base = bids_base
 		get_s_scan.iterables = ("scan_type", structural_scan_types)
-
-		s_bru2nii = pe.Node(interface=bru2nii.Bru2(), name="s_bru2nii")
-		s_bru2nii.inputs.force_conversion=True
-		s_bru2nii.inputs.actual_size=actual_size
 
 		#s_bids_filename = pe.Node(name='s_bids_filename', interface=util.Function(function=sss_filename,input_names=inspect.getargspec(sss_filename)[0], output_names=['filename']))
 		s_bids_filename = pe.Node(name='s_bids_filename', interface=util.Function(function=bids_naming,input_names=inspect.getargspec(bids_naming)[0], output_names=['filename']))
@@ -228,7 +230,7 @@ def bruker(measurements_base, template,
 				workflow_connections.extend([
 					(s_biascorrect, s_register, [('output_image', 'moving_image')]),
 					(s_register, s_warp, [('composite_transform', 'transforms')]),
-					(s_bru2nii, s_warp, [('nii_file', 'input_image')]),
+					(get_s_scan, s_warp, [('nii_path', 'input_image')]),
 					(s_warp, datasink, [('output_image', 'anat')]),
 					])
 		else:
@@ -251,7 +253,7 @@ def bruker(measurements_base, template,
 			s_register, s_warp, f_warp = structural_registration(template)
 
 			workflow_connections.extend([
-				(s_bru2nii, s_reg_biascorrect, [('nii_file', 'input_image')]),
+				(get_s_scan, s_reg_biascorrect, [('nii_path', 'input_image')]),
 				(s_reg_biascorrect, s_cutoff, [('output_image', 'in_file')]),
 				(s_cutoff, s_BET, [('out_file', 'in_file')]),
 				(s_biascorrect, s_mask, [('output_image', 'in_file')]),
@@ -268,7 +270,7 @@ def bruker(measurements_base, template,
 				workflow_connections.extend([
 					(s_mask, s_register, [('out_file', 'moving_image')]),
 					(s_register, s_warp, [('composite_transform', 'transforms')]),
-					(s_bru2nii, s_warp, [('nii_file', 'input_image')]),
+					(get_s_scan, s_warp, [('nii_path', 'input_image')]),
 					(s_warp, datasink, [('output_image', 'anat')]),
 					])
 
@@ -279,10 +281,9 @@ def bruker(measurements_base, template,
 		workflow_connections.extend([
 			(infosource, get_s_scan, [('subject_session', 'selector')]),
 			(infosource, s_bids_filename, [('subject_session', 'subject_session')]),
-			(get_s_scan, s_bru2nii, [('scan_path','input_dir')]),
 			(get_s_scan, s_bids_filename, [('scan_type', 'scan_type')]),
 			(s_bids_filename, s_warp, [('filename','output_image')]),
-			(s_bru2nii, s_biascorrect, [('nii_file', 'input_image')]),
+			(get_s_scan, s_biascorrect, [('nii_path', 'input_image')]),
 			])
 
 	if functional_registration_method == "structural":
@@ -419,7 +420,7 @@ def bruker(measurements_base, template,
 			(f_warp, bandpass, [('output_image', 'in_file')]),
 			])
 
-	workflow_config = {'execution': {'crashdump_dir': path.join(measurements_base,'preprocessing/crashdump'),}}
+	workflow_config = {'execution': {'crashdump_dir': path.join(bids_base,'preprocessing/crashdump'),}}
 	if debug:
 		workflow_config['logging'] = {
 			'workflow_level':'DEBUG',
@@ -432,7 +433,7 @@ def bruker(measurements_base, template,
 	workdir_name = workflow_name+"_work"
 	workflow = pe.Workflow(name=workdir_name)
 	workflow.connect(workflow_connections)
-	workflow.base_dir = path.join(measurements_base,"preprocessing")
+	workflow.base_dir = path.join(bids_base,"preprocessing")
 	workflow.config = workflow_config
 	workflow.write_graph(dotfilename=path.join(workflow.base_dir,workdir_name,"graph.dot"), graph2use="hierarchical", format="png")
 

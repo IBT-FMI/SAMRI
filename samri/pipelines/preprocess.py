952ce5ca488c4c51dba79c55ac738dcb852d9bc5
damaggu <marksm@ethz.ch>
2018-04-05 19:37:48 +0200

    clean up workflow and optional bandpass

--- samri/pipelines/preprocess.py
+++ samri/pipelines/preprocess.py
@@ -1,5 +1,5 @@
 from os import path, listdir, getcwd, remove
-from samri.pipelines.extra_functions import get_data_selection, get_bids_scan, write_bids_metadata_file, write_bids_events_file, force_dummy_scans, BIDS_METADATA_EXTRACTION_DICTS
+from samri.pipelines.extra_functions import get_data_selection, get_bids_scan, write_bids_metadata_file,write_bids_events_file ,write_events_file, force_dummy_scans, BIDS_METADATA_EXTRACTION_DICTS
 
 import re
 import inspect
@@ -43,7 +43,12 @@ def filterData(df, col_name, entries):
 			res_df = res_df.append(_df)
 	return res_df
 
-def bids_data_selection(base, structural_match, functional_match, subjects, sessions):
+def bids_data_selection(base,
+		structural_match,
+		functional_match,
+		subjects,
+		sessions,
+		):
 	validate = BIDSValidator()
 	for x in os.walk(base):
 		print(x[0])
@@ -67,18 +72,16 @@ def bids_data_selection(base, structural_match, functional_match, subjects, sess
 	df.loc[df.modality == 'anat', 'scan_type'] = 'acq-'+df['acq'] +'_' + df['type']
 
 	#TODO: make nicer, generalize to all functional match entries... dict vs list von subjects,etc. und acq / acquistion mismatch
+
 	res_df = pd.DataFrame()
 	if(functional_match):
 		_df = filterData(df, 'task', functional_match['task'])
-		try:
-			_df = filterData(df, 'type', functional_match['type'])
-		except:
-			pass
 		res_df = res_df.append(_df)
 		if(structural_match):
 			_df = filterData(df, 'acq', structural_match['acquisition'])
 			res_df = res_df.append(_df)
 	df = res_df
+
 	if(subjects):
 		df = filterData(df, 'subject', subjects)
 	if(sessions):
@@ -109,6 +112,7 @@ def bruker(bids_base, template,
 	autorotate=False,
 	strict=False,
 	verbose=False,
+	bandpass = False,
 	):
 	'''
 
@@ -149,14 +153,9 @@ def bruker(bids_base, template,
 	struct_ind = _struct_ind.index.tolist()
 	sel = data_selection.index.tolist()
 
-	if True:
+	if(DEBUG):
 		print(data_selection)
 		print(subjects_sessions)
-
-		test = get_bids_scan(bids_base, data_selection, func_ind[0])
-		print(test)
-	#infosource = pe.Node(interface=util.IdentityInterface(fields=['subject_session'], mandatory_inputs=False), name="infosource")
-	#infosource.iterables = [('subject_session', subjects_sessions)]
 
 	get_f_scan = pe.Node(name='get_f_scan', interface=util.Function(function=get_bids_scan,input_names=inspect.getargspec(get_bids_scan)[0], output_names=['scan_path','scan_type','task', 'nii_path', 'nii_name', 'file_name', 'events_name', 'subject_session']))
 	get_f_scan.inputs.ignore_exception = True
@@ -164,7 +163,7 @@ def bruker(bids_base, template,
 	get_f_scan.inputs.bids_base = bids_base
 	get_f_scan.iterables = ("ind_type", func_ind)
 
-	dummy_scans = pe.Node(name='dummy_scans', interface=util.Function(function=force_dummy_scans,input_names=inspect.getargspec(force_dummy_scans)[0], output_names=['out_file','deleted_scans']))
+	dummy_scans = pe.Node(name='dummy_scans', interface=util.Function(function=force_dummy_scans,input_names=inspect.getargspec(force_dummy_scans)[0], output_names=['out_file']))
 	dummy_scans.inputs.desired_dummy_scans = DUMMY_SCANS
 
 	bandpass = pe.Node(interface=fsl.maths.TemporalFilter(), name="bandpass")
@@ -174,20 +173,11 @@ def bruker(bids_base, template,
 	else:
 		bandpass.inputs.lowpass_sigma = tr
 
-	#bids_filename = pe.Node(name='bids_filename', interface=util.Function(function=sss_filename,input_names=inspect.getargspec(sss_filename)[0], output_names=['filename']))
-	bids_filename = pe.Node(name='bids_filename', interface=util.Function(function=bids_naming,input_names=inspect.getargspec(bids_naming)[0], output_names=['filename']))
-	bids_filename.inputs.metadata = data_selection
-
-	#bids_stim_filename = pe.Node(name='bids_stim_filename', interface=util.Function(function=sss_filename,input_names=inspect.getargspec(sss_filename)[0], output_names=['filename']))
-	bids_stim_filename = pe.Node(name='bids_stim_filename', interface=util.Function(function=bids_naming,input_names=inspect.getargspec(bids_naming)[0], output_names=['filename']))
-	bids_stim_filename.inputs.suffix = "events"
-	bids_stim_filename.inputs.extension = ".tsv"
-	bids_stim_filename.inputs.metadata = data_selection
-
-	events_file = pe.Node(name='events_file', interface=util.Function(function=write_bids_events_file,input_names=inspect.getargspec(write_bids_events_file)[0], output_names=['out_file']))
-	#events_file.inputs.very_nasty_bruker_delay_hack = very_nasty_bruker_delay_hack
-	#if not (strict or verbose):
-	#	events_file.inputs.ignore_exception = True
+	events_file = pe.Node(name='events_file', interface=util.Function(function=write_bids_events_file,input_names=inspect.getargspec(write_events_file)[0], output_names=['out_file']))
+	events_file.inputs.dummy_scans_ms = DUMMY_SCANS * tr * 1000
+	events_file.inputs.very_nasty_bruker_delay_hack = very_nasty_bruker_delay_hack
+	if not (strict or verbose):
+		events_file.inputs.ignore_exception = True
 
 	datasink = pe.Node(nio.DataSink(), name='datasink')
 	datasink.inputs.base_directory = path.join(bids_base,"preprocessing",workflow_name)
@@ -196,26 +186,15 @@ def bruker(bids_base, template,
 		datasink.inputs.ignore_exception = True
 
 	workflow_connections = [
-		#(infosource, get_f_scan, [('subject_session', 'selector')]),
-		(get_f_scan, bids_stim_filename, [('subject_session', 'subject_session')]),
-		(get_f_scan, bids_stim_filename, [('scan_type', 'scan_type')]),
 		(get_f_scan, dummy_scans, [('nii_path', 'in_file')]),
 		(get_f_scan, dummy_scans, [('scan_path', 'scan_dir')]),
-		(dummy_scans, events_file, [('deleted_scans', 'forced_dummy_scans')]),
 		(get_f_scan, events_file, [
-			('nii_path', 'timecourse_file'),
 			('task', 'task'),
 			('scan_path', 'scan_dir')
 			]),
 		(events_file, datasink, [('out_file', 'func.@events')]),
 		(get_f_scan, events_file, [('events_name', 'out_file')]),
-		#(bids_stim_filename, events_file, [('filename', 'out_file')]),
 		(get_f_scan, datasink, [(('subject_session',ss_to_path), 'container')]),
-		(get_f_scan, bids_filename, [('subject_session', 'subject_session')]),
-		(get_f_scan, bids_filename, [('scan_type', 'scan_type')]),
-		#(bids_filename, bandpass, [('filename', 'out_file')]),
-		(get_f_scan, bandpass, [('nii_name', 'out_file')]),
-		(bandpass, datasink, [('out_file', 'func')]),
 		]
 
 	if realign == "space":
@@ -252,11 +231,6 @@ def bruker(bids_base, template,
 		get_s_scan.inputs.ignore_exception = True
 		get_s_scan.inputs.data_selection = data_selection
 		get_s_scan.inputs.bids_base = bids_base
-		#get_s_scan.iterables =  ("ind_type", struct_ind)
-
-		#s_bids_filename = pe.Node(name='s_bids_filename', interface=util.Function(function=sss_filename,input_names=inspect.getargspec(sss_filename)[0], output_names=['filename']))
-		s_bids_filename = pe.Node(name='s_bids_filename', interface=util.Function(function=bids_naming,input_names=inspect.getargspec(bids_naming)[0], output_names=['filename']))
-		s_bids_filename.inputs.metadata = data_selection
 
 		if actual_size:
 			s_register, s_warp, _, _ = DSURQEc_structural_registration(template, registration_mask)
@@ -320,10 +294,6 @@ def bruker(bids_base, template,
 
 		workflow_connections.extend([
 			(get_f_scan, get_s_scan, [('subject_session', 'selector')]),
-			#(infosource, get_s_scan, [('subject_session', 'selector')]),
-			(get_f_scan, s_bids_filename, [('subject_session', 'subject_session')]),
-			(get_s_scan, s_bids_filename, [('scan_type', 'scan_type')]),
-			#(s_bids_filename, s_warp, [('filename','output_image')]),
 			(get_s_scan, s_warp, [('nii_name','output_image')]),
 			(get_s_scan, s_biascorrect, [('nii_path', 'input_image')]),
 			])
@@ -458,9 +428,17 @@ def bruker(bids_base, template,
 			(invert, bandpass, [('out_file', 'in_file')]),
 			])
 	else:
-		workflow_connections.extend([
-			(f_warp, bandpass, [('output_image', 'in_file')]),
-			])
+		if bandpass:
+			workflow_connections.extend([
+				(get_f_scan, bandpass, [('nii_name', 'out_file')]),
+				(f_warp, bandpass, [('output_image', 'in_file')]),
+				(bandpass, datasink, [('out_file', 'func')]),
+				])
+		else:
+			workflow_connections.extend([
+				(f_warp, datasink, [('output_image', 'func')]),
+				])
+
 
 	workflow_config = {'execution': {'crashdump_dir': path.join(bids_base,'preprocessing/crashdump'),}}
 	if debug:

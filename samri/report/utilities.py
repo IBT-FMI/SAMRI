e679e004ade039b6a3db6281bfbe1314794903b6
Horea Christian <horea.christ@yandex.com>
2018-04-08 02:15:14 +0200

    Reorganized except structure and added docstring

--- samri/report/utilities.py
+++ samri/report/utilities.py
@@ -36,12 +36,12 @@ def add_roi_data(img_path, masker,
 	img_path = path.abspath(path.expanduser(img_path))
 	try:
 		img = nib.load(img_path)
-		img = masker.fit_transform(img)
-		img = img.flatten()
-		mean = np.nanmean(img)
 	except (FileNotFoundError, nib.py3k.FileNotFoundError):
 		return pd.DataFrame({})
 	else:
+		img = masker.fit_transform(img)
+		img = img.flatten()
+		mean = np.nanmean(img)
 		subject_data['session'] = substitution['session']
 		subject_data['subject'] = substitution['subject']
 		subject_data['t'] = mean
@@ -54,24 +54,42 @@ def add_roi_data(img_path, masker,
 		df = pd.DataFrame(subject_data, index=[None])
 		return df
 
-def add_pattern_data(substitution,img_path,pattern):
-	"""Return a dataframe containing the subject- and session-wise mean of a patern score."""
+def add_pattern_data(img_path, pattern,
+	substitution=False,
+	):
+	"""
+	Return a dataframe containing the subject- and session-wise mean of a patern score.
+
+	Parameters
+	----------
+
+	img_path : str
+		Path to NIfTI file from which the ROI is to be extracted.
+	pattern : nilearn.NiftiMasker
+		Nilearn `nifti1.Nifti1Image` object to use for masking the desired ROI.
+	substitution : dict, optional
+		A dictionary with keys which include 'subject' and 'session'.
+
+	"""
+
 	subject_data={}
 	if substitution:
 		img_path = img_path.format(**substitution)
 	img_path = path.abspath(path.expanduser(img_path))
 	try:
 		img = nib.load(img_path)
-		img_data = img.get_data()
 	except (FileNotFoundError, nib.py3k.FileNotFoundError):
 		return pd.DataFrame({}), pd.DataFrame({})
 	else:
-		pattern_evaluation = img_data*pattern
+		img_data = img.get_data()
+		pattern_data = pattern.get_data()
+		pattern_evaluation = img_data*pattern_data
 		pattern_evaluation = pattern_evaluation.flatten()
 		pattern_score = np.nanmean(pattern_evaluation)
 		subject_data["session"]=substitution["session"]
 		subject_data["subject"]=substitution["subject"]
 		subject_data["t"]=pattern_score
+		subject_data['feature'] = pattern.get_filename()
 		df = pd.DataFrame(subject_data, index=[None])
 		return df
 

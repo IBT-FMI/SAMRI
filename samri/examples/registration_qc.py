6c42d03ab1254dcb959fc442ddd3b84c0b88c2dd
damaggu <marksm@ethz.ch>
2018-05-31 23:31:12 +0200

    added registration eval via variance for different metrics

--- samri/examples/registration_qc.py
+++ samri/examples/registration_qc.py
@@ -4,6 +4,8 @@ import statsmodels.formula.api as smf
 from os import path
 from samri.plotting.aggregate import registration_qc
 from samri.typesetting import inline_anova
+from math import sqrt
+import matplotlib.pyplot as plt
 
 data_dir = path.join(path.dirname(path.realpath(__file__)),"../../example_data/")
 df_path = path.join(data_dir,"f_reg_quality.csv")
@@ -29,3 +31,26 @@ print(inline_anova(anova_summary,"C(session)",style="python", max_len=2))
 print(inline_anova(anova_summary,"C(subject)",style="tex"))
 print(inline_anova(anova_summary,"C(session)",style="tex", max_len=2))
 #print(inline_anova(anova_summary,"C(acquisition)",style="tex"))
+
+
+# plotting variance results for different metrics
+# TODO: warum MI = 0?
+
+metrics = ['CC', 'MI', 'GC']
+means = []
+stds = []
+
+for metric in metrics:
+
+	df_path = path.join('/media/nexus/storage/coding/github/SAMRI/',"f_reg_quality_"+metric+".csv")
+	df = pd.read_csv(df_path)
+	variances = df.groupby('subject').agg('var')
+	std = sqrt(variances['similarity'].sum())
+	mean = df.groupby('subject').agg('mean')['similarity'].sum()
+	means.append(mean)
+	stds.append(std)
+
+
+print(metrics)
+print(means)
+print(stds)

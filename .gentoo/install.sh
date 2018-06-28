e07086396155380569b36e9f9efbcb56b89ac261
Horea Christian <horea.christ@yandex.com>
2018-06-28 03:59:38 +0200

    pulling in any remaining deps

--- .gentoo/install.sh
+++ .gentoo/install.sh
@@ -13,4 +13,4 @@ EBUILD="$(find . -name "*.ebuild" | head -n1)"
 echo "Installing ebuild $EBUILD"
 
 localexec ebuild "$EBUILD" manifest
-localexec emerge "$@" "$EBUILD"
+localexec emerge "$@" "$EBUILD" --autounmask-continue

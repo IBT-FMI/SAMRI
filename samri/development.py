77555a9a1ed17435848513b2de0e637f383f73c8
damaggu <marksm@ethz.ch>
2018-07-19 13:31:23 +0200

    fixing radius

--- samri/development.py
+++ samri/development.py
@@ -265,7 +265,7 @@ def test_reg_qc(
 def reg_cc(
         path = "~/ni_data/ofM.dr/preprocessing/composite",
         template = "/usr/share/mouse-brain-atlases/dsurqec_200micron.nii",
-        radius=5,
+        radius=8,
         autofind=False,
         plot=False,
         save = "f_reg_quality",

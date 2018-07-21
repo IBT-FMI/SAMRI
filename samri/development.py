60e0c6b7bb083ecc8b57b98581bd7748be00ad5a
Horea Christian <h.chr@mail.ru>
2018-07-21 23:58:12 +0200

    Updated development examples

--- samri/development.py
+++ samri/development.py
@@ -166,50 +166,12 @@ def irsabi():
 		out_base='~/ni_data/ofM.dr/preprocessing',
 		)
 	legacy(bids_base, "/usr/share/mouse-brain-atlases/lambmc_200micron.nii",
-		functional_match={'type':['cbv'],},
-		structural_match={'acquisition':['TurboRARElowcov'],},
+		functional_match={'type':['cbv'],'acquisition':['EPIlowcov']},
 		negative_contrast_agent=True,
 		out_base='~/ni_data/ofM.dr/preprocessing',
 		)
 	legacy(bids_base, "/usr/share/mouse-brain-atlases/lambmc_200micron.nii",
-		functional_match={'type':['bold'],},
-		structural_match={'acquisition':['TurboRARElowcov'],},
-		out_base='~/ni_data/ofM.dr/preprocessing',
-		)
-def irsabi_gen():
-	from samri.pipelines.preprocess import generic, legacy
-
-	bids_base = '~/ni_data/ofM.dr/bids'
-	generic(bids_base, "/usr/share/mouse-brain-atlases/dsurqec_200micron.nii",
-		registration_mask="/usr/share/mouse-brain-atlases/dsurqec_200micron_mask.nii",
-		functional_match={'type':['cbv'],},
-		structural_match={'acquisition':['TurboRARElowcov'],},
-		actual_size=True,
-		functional_registration_method="composite",
-		negative_contrast_agent=True,
-		out_base='~/ni_data/ofM.dr/preprocessing',
-		)
-	generic(bids_base, "/usr/share/mouse-brain-atlases/dsurqec_200micron.nii",
-		registration_mask="/usr/share/mouse-brain-atlases/dsurqec_200micron_mask.nii",
-		functional_match={'type':['bold'],},
-		structural_match={'acquisition':['TurboRARElowcov'],},
-		actual_size=True,
-		functional_registration_method="composite",
-		out_base='~/ni_data/ofM.dr/preprocessing',
-		)
-def irsabi_leg():
-	from samri.pipelines.preprocess import generic, legacy
-
-	bids_base = '~/ni_data/ofM.dr/bids'
-	legacy(bids_base, "/usr/share/mouse-brain-atlases/lambmc_200micron.nii",
-		functional_match={'type':['cbv'],},
-		structural_match={'acquisition':['TurboRARElowcov'],},
-		negative_contrast_agent=True,
-		out_base='~/ni_data/ofM.dr/preprocessing',
-		)
-	legacy(bids_base, "/usr/share/mouse-brain-atlases/lambmc_200micron.nii",
-		functional_match={'type':['bold'],},
-		structural_match={'acquisition':['TurboRARElowcov'],},
+		functional_match={'type':['bold'],'acquisition':['EPIlowcov']},
 		out_base='~/ni_data/ofM.dr/preprocessing',
 		)
 

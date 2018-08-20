9e401b0d4988843753d024176c3934ae694d1fb0
Horea Christian <horea.christ@yandex.com>
2018-08-20 23:53:28 +0200

    Improved processor scaling

--- samri/pipelines/manipulations.py
+++ samri/pipelines/manipulations.py
@@ -3,7 +3,8 @@ import re
 
 from samri.utilities import iter_collapse_by_path
 
-def collapse_nifti(in_dir, out_dir):
+def collapse_nifti(in_dir, out_dir,
+	**kwargs):
 	in_dir = os.path.abspath(os.path.expanduser(in_dir))
 	out_dir = os.path.abspath(os.path.expanduser(out_dir))
 	in_files = []
@@ -16,4 +17,6 @@ def collapse_nifti(in_dir, out_dir):
 	# Make relative to `in_dir`:
 	out_files = [re.sub(r'^' + re.escape(in_dir), '', i) for i in in_files]
 	out_files = [os.path.join(out_dir,i[1:]) for i in out_files]
-	out_files = iter_collapse_by_path(in_files, out_files)
+	out_files = iter_collapse_by_path(in_files, out_files,
+		**kwargs,
+		)

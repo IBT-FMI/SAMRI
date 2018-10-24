64914c190d12f754e5decd6b77537756fbed1a34
Horea Christian <h.chr@mail.ru>
2018-10-24 18:31:48 +0200

    Implemented index iteration for dwi scans

--- samri/pipelines/reposit.py
+++ samri/pipelines/reposit.py
@@ -130,6 +130,7 @@ def bru2bids(measurements_base,
 			measurements=measurements,
 			)
 		diffusion_scan_types = list(d_data_selection['scan_type'].unique())
+		dwi_ind = d_data_selection.index.tolist()
 		data_selection = pd.concat([data_selection,d_data_selection])
 
 	# we start to define nipype workflow elements (nodes, connections, meta)
@@ -223,41 +224,70 @@ def bru2bids(measurements_base,
 				pass
 
 	if diffusion_scan_types:
-		get_d_scan = pe.Node(name='get_d_scan', interface=util.Function(function=get_scan, input_names=inspect.getargspec(get_scan)[0], output_names=['scan_path','scan_type','task']))
+		get_d_scan = pe.Node(name='get_d_scan', interface=util.Function(function=get_bids_scan,input_names=inspect.getargspec(get_bids_scan)[0], output_names=[
+			'scan_path', 'typ', 'task', 'nii_path', 'nii_name', 'eventfile_name', 'subject_session', 'metadata_filename'
+			]))
 		get_d_scan.inputs.ignore_exception = True
-		get_d_scan.inputs.data_selection = data_selection
-		get_d_scan.inputs.measurements_base = measurements_base
-		get_d_scan.iterables = ("scan_type", diffusion_scan_types)
+		get_d_scan.inputs.data_selection = f_data_selection
+		get_d_scan.inputs.extra = ['acq']
+		get_d_scan.inputs.bids_base = measurements_base
+		get_d_scan.iterables = ("ind_type", dwi_ind)
 
 		d_bru2nii = pe.Node(interface=bru2nii.Bru2(), name="d_bru2nii")
 		d_bru2nii.inputs.force_conversion=True
 		d_bru2nii.inputs.actual_size = not inflated_size
 
-		d_filename = pe.Node(name='d_filename', interface=util.Function(function=bids_naming,input_names=inspect.getargspec(bids_naming)[0], output_names=['filename']))
-		d_filename.inputs.metadata = data_selection
-		d_filename.inputs.extension=''
-
-		d_metadata_filename = pe.Node(name='d_metadata_filename', interface=util.Function(function=bids_naming,input_names=inspect.getargspec(bids_naming)[0], output_names=['filename']))
-		d_metadata_filename.inputs.extension = ".json"
-		d_metadata_filename.inputs.metadata = data_selection
-
-		d_metadata_file = pe.Node(name='d_metadata_file', interface=util.Function(function=write_bids_metadata_file,input_names=inspect.getargspec(write_bids_metadata_file)[0], output_names=['out_file']))
+		d_metadata_file = pe.Node(name='metadata_file', interface=util.Function(function=write_bids_metadata_file,input_names=inspect.getargspec(write_bids_metadata_file)[0], output_names=['out_file']))
 		d_metadata_file.inputs.extraction_dicts = BIDS_METADATA_EXTRACTION_DICTS
 
 		workflow_connections = [
-			(infosource, datasink, [(('subject_session',ss_to_path), 'container')]),
-			(infosource, get_d_scan, [('subject_session', 'selector')]),
-			(infosource, d_filename, [('subject_session', 'subject_session')]),
-			(infosource, d_metadata_filename, [('subject_session', 'subject_session')]),
-			(get_d_scan, d_bru2nii, [('scan_path','input_dir')]),
-			(get_d_scan, d_filename, [('scan_type', 'scan_type')]),
-			(get_d_scan, d_metadata_filename, [('scan_type', 'scan_type')]),
-			(get_d_scan, d_metadata_file, [('scan_path', 'scan_dir')]),
-			(d_filename, d_bru2nii, [('filename','output_filename')]),
-			(d_metadata_filename, d_metadata_file, [('filename', 'out_file')]),
+			(get_d_scan, datasink, [(('subject_session',ss_to_path), 'container')]),
+			(get_d_scan, d_bru2nii, [('scan_path', 'input_dir')]),
+			(get_d_scan, d_bru2nii, [('nii_name', 'output_filename')]),
 			(d_bru2nii, datasink, [('nii_file', 'dwi')]),
-			(d_metadata_file, datasink, [('out_file', 'dwi.@metadata')]),
+			(get_d_scan, d_metadata_file, [
+				('metadata_filename', 'out_file'),
+				('task', 'task'),
+				('scan_path', 'scan_dir')
+				]),
+			(d_metadata_file, datasink, [('out_file', 'func.@metadata')]),
 			]
+
+		#get_d_scan = pe.Node(name='get_d_scan', interface=util.Function(function=get_scan, input_names=inspect.getargspec(get_scan)[0], output_names=['scan_path','scan_type','task']))
+		#get_d_scan.inputs.ignore_exception = True
+		#get_d_scan.inputs.data_selection = data_selection
+		#get_d_scan.inputs.measurements_base = measurements_base
+		#get_d_scan.iterables = ("scan_type", diffusion_scan_types)
+
+		#d_bru2nii = pe.Node(interface=bru2nii.Bru2(), name="d_bru2nii")
+		#d_bru2nii.inputs.force_conversion=True
+		#d_bru2nii.inputs.actual_size = not inflated_size
+
+		#d_filename = pe.Node(name='d_filename', interface=util.Function(function=bids_naming,input_names=inspect.getargspec(bids_naming)[0], output_names=['filename']))
+		#d_filename.inputs.metadata = data_selection
+		#d_filename.inputs.extension=''
+
+		#d_metadata_filename = pe.Node(name='d_metadata_filename', interface=util.Function(function=bids_naming,input_names=inspect.getargspec(bids_naming)[0], output_names=['filename']))
+		#d_metadata_filename.inputs.extension = ".json"
+		#d_metadata_filename.inputs.metadata = data_selection
+
+		#d_metadata_file = pe.Node(name='d_metadata_file', interface=util.Function(function=write_bids_metadata_file,input_names=inspect.getargspec(write_bids_metadata_file)[0], output_names=['out_file']))
+		#d_metadata_file.inputs.extraction_dicts = BIDS_METADATA_EXTRACTION_DICTS
+
+		#workflow_connections = [
+		#	(infosource, datasink, [(('subject_session',ss_to_path), 'container')]),
+		#	(infosource, get_d_scan, [('subject_session', 'selector')]),
+		#	(infosource, d_filename, [('subject_session', 'subject_session')]),
+		#	(infosource, d_metadata_filename, [('subject_session', 'subject_session')]),
+		#	(get_d_scan, d_bru2nii, [('scan_path','input_dir')]),
+		#	(get_d_scan, d_filename, [('scan_type', 'scan_type')]),
+		#	(get_d_scan, d_metadata_filename, [('scan_type', 'scan_type')]),
+		#	(get_d_scan, d_metadata_file, [('scan_path', 'scan_dir')]),
+		#	(d_filename, d_bru2nii, [('filename','output_filename')]),
+		#	(d_metadata_filename, d_metadata_file, [('filename', 'out_file')]),
+		#	(d_bru2nii, datasink, [('nii_file', 'dwi')]),
+		#	(d_metadata_file, datasink, [('out_file', 'dwi.@metadata')]),
+		#	]
 		crashdump_dir = path.join(out_base,workflow_name+'_crashdump')
 		workflow_config = {'execution': {'crashdump_dir': crashdump_dir}}
 		if debug:

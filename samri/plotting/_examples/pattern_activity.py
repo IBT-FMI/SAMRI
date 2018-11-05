cabe81af217f9c55f19f0dae5a90c9c17dda9aa6
Horea Christian <horea.christ@yandex.com>
2018-11-05 08:13:14 +0100

    Updated to not use behaviopy

--- samri/plotting/_examples/pattern_activity.py
+++ samri/plotting/_examples/pattern_activity.py
@@ -4,14 +4,24 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import samri.plotting.maps as maps
-from behaviopy.plotting import qualitative_times
+import seaborn as sns
 from os import path
 
-data_dir = path.join(path.dirname(path.realpath(__file__)),"../../example_data/ioanas2018")
+data_dir = path.join(path.dirname(path.realpath(__file__)),"../../../example_data/ioanas2018")
 data_path = path.join(data_dir,'pattern_summary.csv')
 df = pd.read_csv(data_path)
 
+# Style elements
+palette=["#56B4E9", "#E69F00"]
+
 df = df.rename(columns={'t':'Arbitrary Units'})
+df['Session']=df['Session'].map({
+	'ofM':'naïve',
+	'ofMaF':'acute',
+	'ofMcF1':'chronic/2w',
+	'ofMcF2':'chronic/4w',
+	'ofMpF':'post',
+	})
 
 # definitions for the axes
 left, width = 0.06, 0.9
@@ -23,38 +33,32 @@ roi_coordinates = [left+0.45, bottom+0.7, 0.3, 0.2]
 fig = plt.figure(1)
 
 ax1 = plt.axes(session_coordinates)
-qualitative_times(df,
-        ax=ax1,
-        x='Session',
-        y='Arbitrary Units',
-        condition='treatment',
-        unit='subject',
-        ci=90,
-        palette=["#56B4E9", "#E69F00"],
-        order=['naïve','acute','chronic (2w)','chronic (4w)','post'],
-        bp_style=False,
-        renames={
-                'Session':{
-                        'ofM':'naïve',
-                        'ofMaF':'acute',
-                        'ofMcF1':'chronic (2w)',
-                        'ofMcF2':'chronic (4w)',
-                        'ofMpF':'post',
-                        },
-                },
-        )
+sns.pointplot(
+       x='Session',
+       y='Arbitrary Units',
+       units='subject',
+       data=df,
+       hue='treatment',
+       dodge=True,
+       palette=palette,
+       order=['naïve','acute','chronic/2w','chronic/4w','post'],
+       ax=ax1,
+       ci=95,
+       )
 
-ax2 = plt.axes(roi_coordinates)
-stat_map = path.abspath(path.expanduser('~/ni_data/ofM.dr/bids/l2/best_responders/sessionofM/tstat1.nii.gz'))
-template = path.abspath(path.expanduser('/usr/share/mouse-brain-atlases/dsurqec_40micron_masked.nii'))
-maps.stat(stat_maps=[stat_map],
-        template=template,
-        cut_coords=[(0,-4.3,-3.3)],
-        annotate=False,
-        scale=0.3,
-        show_plot=False,
-        interpolation=None,
-        threshold=4,
-        draw_colorbar=False,
-        ax=ax2,
-        )
+#ax2 = plt.axes(roi_coordinates)
+#stat_map = path.abspath(path.expanduser('~/ni_data/ofM.dr/bids/l2/best_responders/sessionofM/tstat1.nii.gz'))
+#template = path.abspath(path.expanduser('/usr/share/mouse-brain-atlases/dsurqec_40micron_masked.nii'))
+#maps.stat(stat_maps=[stat_map],
+#	template=template,
+#	cut_coords=[(0,-4.3,-3.3)],
+#	annotate=False,
+#	scale=0.3,
+#	show_plot=False,
+#	interpolation=None,
+#	threshold=4,
+#	draw_colorbar=False,
+#	ax=ax2,
+#	)
+
+plt.savefig('pattern_activity.png')

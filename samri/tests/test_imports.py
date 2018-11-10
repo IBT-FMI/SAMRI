eb21537c4f89ca61eb36d73154d443157951c2bb
Horea Christian <horea.christ@yandex.com>
2018-11-10 20:59:33 +0100

    Removed deprecated notice and submodule

--- samri/tests/test_imports.py
+++ samri/tests/test_imports.py
@@ -3,8 +3,7 @@ from samri import utilities
 from samri.analysis import *
 from samri.optimization import *
 from samri.pipelines import *
-# leaving out network because of graph_tool
-from samri.plotting import connectivity,examples,maps,qc,summary,timeseries,utilities
+from samri.plotting import connectivity,maps,qc,summary,timeseries,utilities
 from samri.report import *
 from samri import utilities
 

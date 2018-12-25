e5b0195f94c7f71efb5605e7d49ec2040a27771c
Horea Christian <horea.christ@yandex.com>
2018-12-25 20:56:57 +0100

    Test for roi transformation between templates

---
+++ samri/pipelines/tests/test_manipulations.py
@@ -0,0 +1,10 @@
+from samri.pipelines.manipulations import transform_feature
+
+def test_transform_feature():
+	transform_feature('/usr/share/mouse-brain-atlases/dsurqec_200micron_roi-dr.nii', '/usr/share/mouse-brain-atlases/dsurqec_200micron_masked.nii', '/usr/share/mouse-brain-atlases/ambmc_200micron.nii',
+		target_mask='',
+		phases=['rigid','affine','syn'],
+		num_threads=4,
+		output_name='transformed_feature.nii.gz',
+		interpolation='NearestNeighbor',
+		)

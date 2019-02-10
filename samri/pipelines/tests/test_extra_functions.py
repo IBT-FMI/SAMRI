5d5a19a4b9c220e56c25f6edc2bc2019fee14ee2
Horea Christian <horea.christ@yandex.com>
2019-02-10 20:13:37 +0100

    Adapted to new preprocessing data

--- samri/pipelines/tests/test_extra_functions.py
+++ samri/pipelines/tests/test_extra_functions.py
@@ -2,7 +2,7 @@ def test_reset_background():
 	from samri.pipelines.extra_functions import reset_background
 	import nibabel as nib
 
-	reset_background('/usr/share/samri_bidsdata/preprocessing/generic/sub-4007/ses-ofM/func/sub-4007_ses-ofM_task-JogB_acq-EPIlowcov_run-1_cbv.nii.gz',
+	reset_background('/usr/share/samri_bidsdata/preprocessing/sub-4007/ses-ofM/func/sub-4007_ses-ofM_task-JogB_acq-EPIlowcov_run-1_cbv.nii.gz',
 		restriction_range=10,
 		bg_value=1000,
 		out_file='reset_background.nii.gz'

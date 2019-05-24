3abd4fdfd66964d96741bb26f3649817336711d9
Horea Christian <chr@chymera.eu>
2019-05-24 17:11:42 +0200

    Not writing output to ram

--- test_scripts.sh
+++ test_scripts.sh
@@ -1,1 +1,1 @@
-SAMRI bru2bids -o /tmp/samri_testing/bash -f '{"acquisition":["EPI"]}' -s '{"acquisition":["TurboRARE"]}' /usr/share/samri_bindata
+SAMRI bru2bids -o /var/tmp/samri_testing/bash -f '{"acquisition":["EPI"]}' -s '{"acquisition":["TurboRARE"]}' /usr/share/samri_bindata

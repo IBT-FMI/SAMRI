771cc810d5fc4568c156d3010544042215c55e2f
Horea Christian <chr@chymera.eu>
2019-05-29 20:35:38 +0200

    Added tests analogous to AOWSIS article

--- test_scripts.sh
+++ test_scripts.sh
@@ -1,1 +1,3 @@
-SAMRI bru2bids -o /var/tmp/samri_testing/bash -f '{"acquisition":["EPI"]}' -s '{"acquisition":["TurboRARE"]}' /usr/share/samri_bindata
+SAMRI bru2bids -o /var/tmp/samri_testing/bash/mouse -f '{"acquisition":["EPI"]}' -s '{"acquisition":["TurboRARE"]}' /usr/share/samri_bindata
+SAMRI bru2bids -o /var/tmp/samri_testing/bash/rat -f '{"acquisition":["seEPI"]}' /usr/share/samri_bindata
+SAMRI bru2bids -o /var/tmp/samri_testing/bash/lemur -f '{"acquisition":["geEPI"]}' -s '{"acquisition":["MSME200um"]}' /usr/share/samri_bindata

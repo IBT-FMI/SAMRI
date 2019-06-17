4f2ac50a93bd895b9ce30db09ab666abcb316883
Horea Christian <horea.christ@yandex.com>
2019-06-17 04:05:26 +0200

    Reordering script for unsupervised classification

--- samri/analysis/segmentation.py
+++ samri/analysis/segmentation.py
@@ -1,17 +1,35 @@
 import numpy as np
-from sklearn.mixture import GMM
+from sklearn.mixture import GaussianMixture
 
 def assign_gaussian(data, n_components, covariance_type,
-	init_params='wc',
-	n_iter=50,
+	init_params='kmeans',
+	n_init=50,
 	):
-	classifier = GMM(
+	classifier = GaussianMixture(
 		n_components=n_components,
 		covariance_type=covariance_type,
 		init_params=init_params,
-		n_iter=n_iter,
+		n_init=n_init,
 		)
 	classifier.fit(data)
 	assignment = classifier.predict(data)
 	return assignment, classifier
 
+def sort_by_occurence(assignments):
+	"""Change unique values in array to ordinal integers based on the number of occurences.
+	Parameters
+	----------
+
+	assignments: array
+		1-D array of values.
+	"""
+
+	labels, counts = np.unique(assignments, return_counts=True)
+	mysort = np.argsort(counts)[::-1]
+	counts = counts[mysort]
+	labels_ = labels[mysort]
+	new_labels = [i for i in range(len(labels))]
+	convert = dict(zip(labels_, new_labels))
+	keys,inv = np.unique(assignments,return_inverse = True)
+	assignments = np.array([convert[x] for x in keys])[inv].reshape(assignments.shape)
+	return assignments

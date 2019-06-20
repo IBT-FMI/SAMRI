af230c6d7f76131ad3872fcbc9682b489c2c6bf8
Horea Christian <horea.christ@yandex.com>
2019-06-20 17:56:55 +0200

    improved structure

--- samri/fetch/local.py
+++ samri/fetch/local.py
@@ -14,7 +14,7 @@ def roi_from_atlaslabel(atlas, label_names,
 		"valuecolumn right":"right label",
 		"valuecolumn left":"left label",
 		},
-	save_as="",
+	save_as='',
 	):
 	"""Return a region of interest map based on an atlas and a label.
 
@@ -35,24 +35,21 @@ def roi_from_atlaslabel(atlas, label_names,
 			components.append(i_data)
 		roi_data = sum(components).astype(bool).astype(int)
 		roi = nib.Nifti1Image(roi_data, atlas.affine, atlas.header)
-
-		return roi
-
-
-	mapping = path.abspath(path.expanduser(mapping))
-	mapping = pd.read_csv(mapping)
-	if bilateral and (parser["valuecolumn right"] and parser["valuecolumn left"]):
-		roi_values = []
-		for label_name in label_names:
-			roi_values.extend(mapping[mapping[parser["textcolumn"]].str.contains(label_name)][parser["valuecolumn right"]].values.tolist())
-			roi_values.extend(mapping[mapping[parser["textcolumn"]].str.contains(label_name)][parser["valuecolumn left"]].values.tolist())
-	header = atlas.header
-	affine = atlas.affine
-	data = atlas.get_data()
-	masked_data = np.in1d(data, roi_values).reshape(data.shape).astype(int)
-	if dilate:
-		masked_data = ndimage.binary_dilation(masked_data).astype(masked_data.dtype)
-	roi = nib.Nifti1Image(masked_data, affine, header)
+	else:
+		mapping = path.abspath(path.expanduser(mapping))
+		mapping = pd.read_csv(mapping)
+		if bilateral and (parser["valuecolumn right"] and parser["valuecolumn left"]):
+			roi_values = []
+			for label_name in label_names:
+				roi_values.extend(mapping[mapping[parser["textcolumn"]].str.contains(label_name)][parser["valuecolumn right"]].values.tolist())
+				roi_values.extend(mapping[mapping[parser["textcolumn"]].str.contains(label_name)][parser["valuecolumn left"]].values.tolist())
+		header = atlas.header
+		affine = atlas.affine
+		data = atlas.get_data()
+		masked_data = np.in1d(data, roi_values).reshape(data.shape).astype(int)
+		if dilate:
+			masked_data = ndimage.binary_dilation(masked_data).astype(masked_data.dtype)
+		roi = nib.Nifti1Image(masked_data, affine, header)
 	if save_as:
 		roi.to_filename(path.abspath(path.expanduser(save_as)))
 

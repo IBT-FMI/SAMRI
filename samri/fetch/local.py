a3bb0ba381bd8a24b2e6a2b58677a3fde008c9f1
Horea Christian <chr@chymera.eu>
2019-09-11 22:36:56 +0200

    Added new function for summary atlases

--- samri/fetch/local.py
+++ samri/fetch/local.py
@@ -5,10 +5,84 @@ from copy import deepcopy
 from os import path
 from scipy import ndimage
 
+def summary_atlas(atlas,
+	mapping='',
+	label_column_l='left label',
+	label_column_r='right label',
+	structure_column='Structure',
+	save_as='',
+	summary={
+		1:{
+			'structure':'Cortex',
+			'summarize':['cortex'],
+			'laterality':'right',
+			},
+		2:{
+			'structure':'Cortex',
+			'summarize':['cortex'],
+			'laterality':'left',
+			},
+		},
+	):
+	"""Return an atlas with labels corresponding to summary categories
+
+	Parameters
+	----------
+	atlas : str, optional
+		Path to NIfTI file containing integer values.
+	mapping : str, optional
+		Path to CSV file which contains columns matching the values assigned to the `label_column_l`, `label_column_r`, `structure_column` parameters of this function.
+	summary : dict, optional
+		Dictionary with keys which are integers, and values which are dictionaries, whose keys in turn are:
+			* 'structure': accepting any value assignement
+			* 'summarize': the value of which is a list of strings which are present on the `mapping` data file column entitled according to `structure_column`.
+			* 'laterality': the value of which is one of {'','both','left','right'}
+	"""
+
+	new_mapping = []
+	new_atlas = None
+	for key in summary:
+
+		metadata = summary[key]
+		new_structure = {}
+		new_structure[structure_column] = metadata['structure']
+		if metadata['laterality'] == 'left':
+			new_structure[label_column_l] = key
+		elif metadata['laterality'] == 'right':
+			new_structure[label_column_r] = key
+		elif metadata['laterality'] in ['','both']:
+			new_structure[label_column_l] = key
+			new_structure[label_column_r] = key
+		new_mapping.append(new_structure)
+
+		roi = roi_from_atlaslabel(atlas, metadata['summarize'],
+			mapping=mapping,
+			label_column_l='left label',
+			label_column_r='right label',
+			structure_column='Structure',
+			output_label=key,
+			laterality=metadata['laterality'],
+			)
+		if new_atlas:
+			header = new_atlas.header
+			affine = new_atlas.affine
+			data = new_atlas.get_data()
+			roi_data = roi.get_data()
+			new_atlas_data = data + roi_data
+			new_atlas = nib.Nifti1Image(new_atlas_data, affine, header)
+		else:
+			new_atlas = roi
+	df = pd.DataFrame(new_mapping)
+	df = df.groupby(structure_column).sum()
+	new_mapping = df.astype({label_column_l: int, label_column_r: int})
+
+	return new_atlas, new_mapping
+
+
 def roi_from_atlaslabel(atlas, label_names,
 	mapping=False,
 	laterality='',
-	dilate=True,
+	dilate=False,
 	label_column_l='left label',
 	label_column_r='right label',
 	structure_column='Structure',

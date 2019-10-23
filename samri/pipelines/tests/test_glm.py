fe544ef4d3c1fa150e3b63419332669246d5a54f
Horea Christian <chr@chymera.eu>
2019-10-23 18:45:07 +0200

    Added test for first-level GLM workflow

--- samri/pipelines/tests/test_glm.py
+++ samri/pipelines/tests/test_glm.py
@@ -1,7 +1,15 @@
-from samri.pipelines.glm import seed
+from samri.pipelines.glm import l1, seed
+
+def test_l1():
+	l1('/var/tmp/samri_testing/pytest/prep',
+		mask='mouse',
+		match={"acq":["EPIlowcov"]},
+		out_base='/var/tmp/samri_testing/pytest/',
+		workflow_name='l1',
+		)
 
 def test_seed():
-	seed('/var/tmp/samri_testing/pytest/preprocessed','/usr/share/mouse-brain-atlases/dsurqec_200micron_roi-dr.nii',
+	seed('/usr/share/samri_bidsdata/preprocessing','/usr/share/mouse-brain-atlases/dsurqec_200micron_roi-dr.nii',
 		match={"acq":["EPIlowcov"]},
 		out_base='/var/tmp/samri_testing/pytest/',
 		workflow_name='dr_fc',

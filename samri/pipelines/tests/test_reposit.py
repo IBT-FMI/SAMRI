3235bac81f81a7783fe66031bc1e7d58ede7bc4f
Horea Christian <chr@chymera.eu>
2019-12-05 07:57:32 +0000

    Reverted to system-wide testing data

--- samri/pipelines/tests/test_reposit.py
+++ samri/pipelines/tests/test_reposit.py
@@ -1,9 +1,8 @@
 from samri.pipelines.reposit import bru2bids
 
 BRU_DIR = '/usr/share/samri_bindata/'
-
-BRU_DIR = '~/samri_bindata'
 # The `20180730_053743_6587_1_1` measurement is acquired with the (incorrect) Bruker ParaVision Prone (i.e. supine) orientation.
+# This is relevant for testing the flip_if_needed function
 
 def test_bru2bids():
 	bru2bids(BRU_DIR,
